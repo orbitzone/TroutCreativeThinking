ed2091297d7056c1c8abad867e07f3d2f8c826cc
joel stewart <joel@trout.com.au>
2013-08-06 15:32:29 +1000

    Cleaned up spacing in outlet file

--- outlet/article/index.php
+++ outlet/article/index.php
@@ -82,17 +82,13 @@
 							<ul class="article-actions span3">
 								<li><a href="javascript:window.print()" class="btn btn-large btn-block"><i class="icon-print"></i> Print</a></li>
 
-
-
-
 								<li>
-									<!-- <a href="#" type="button" data-toggle="dropdown" class="btn btn-large btn-block"><i class="icon-share-sign"></i> Share</a> -->
 									<div class="btn-group btn-block">
 									    <a class="btn btn-large btn-block" data-toggle="dropdown" href="/bathrooms/happy-tips/avoid-the-headbutt#">
 									        <i class="icon-share-alt"></i> Share
 									        <span class="caret"></span>
 									    </a>
-									    <!-- <div class="btn-group"> -->
+	
 									    <ul class="dropdown-menu">
 									        <li><a href="http://www.facebook.com/share.php?u=http://www.reece.com.au/bathrooms/happy-tips/avoid-the-headbutt" onclick="return fbs_click()" title="Facebook" rel="nofollow"><i class="icon-facebook icon-large"></i> Facebook</a></li>
 									        <li><a href="https://twitter.com/intent/tweet?url=http://www.reece.com.au/bathrooms/happy-tips/avoid-the-headbutt" title="Twitter" data-count="none"><i class="icon-twitter icon-large"></i> Twitter</a></li>
@@ -100,13 +96,6 @@
 									    </ul>    
 									</div>
 								</li>
-
-
-
-
-
-
-
 
 								<li><a href="#" class="label">Plumbing</a><a href="#" class="label">Catalogue</a><a href="#" class="label">Tool</a><a href="#" class="label">Media</a><a href="#" class="label">Tag Six</a><a href="#" class="label">Tag Five</a></li>
 							</ul>

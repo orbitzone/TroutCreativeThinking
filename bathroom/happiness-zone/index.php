8aaf36aeea2fea776342df8b5ffe2007374d5577
joel stewart <joel@trout.com.au>
2013-08-14 09:28:10 +1000

    Fixes for a lingering issue on happiness zone template from previous developer

---
+++ bathroom/happiness-zone/index.php
@@ -0,0 +1,175 @@
+
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/variables/variables.php"); ?>
+
+	<?php 
+	  	$cssScriptPaths = array(
+	  		"/assets/css/reece-bathrooms.css"			
+	  	); 
+	  	include_once($serverBase."/includes/head/head-generic.php");
+	?>		
+
+    <body class="hz-landing no-touch">
+	<div class="ocmain-wrapper">
+	    <div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+		    <div class="navbar-container">
+			<a class="btn btn-navbar hidden-desktop" title="button">
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			</a>
+			<a class="brand" href="#" title="Reece">Reece</a>
+			<div class="visible-desktop">
+			    <ul class="nav">
+				<li class="active"><a href="#" title="Bathrooms">Bathrooms</a></li>
+				<li><a href="#" title="Plumbing">Plumbing</a></li>
+				<li><a href="#" title="Specialised Solutions">Specialised Solutions</a></li>
+				<li><a href="#" title="Sustainability">Sustainability</a></li>
+				<li><a href="#" title="About us">About Us</a></li>
+				<li><a href="#" title="Store Finder">Store Finder</a></li>
+				<li><a href="#" title="My Account">My Account</a></li>
+			    </ul>
+			</div>
+		    </div>
+		</div>
+	    </div>
+
+	    <div class="row-fluid main-header">
+		<div class="header-container">
+		    <div class="span6">
+			<div class="title-wrapper">
+			    <h1><a href="happiness-zone" title="Reece Happiness Zone"><img src="/assets/images/title-happiness-zone.png" alt="Happiness Zone" /></a></h1>
+			    <p>All the tools and resources you need to create your perfect bathroom.</p>
+			</div>
+		    </div>
+		    <div class="span6">
+			<ul class="nav nav-pills-span3 visible-desktop">
+			    <li><a href="#" title="Products">Products</a></li>
+			    <li><a href="#" title="Bathroom Planner">Bathroom Planner</a></li>
+			    <li><a href="#" title="Brochures">Brochures</a></li>
+			    <li><a href="#" title="Inspiration">Inspiration</a></li>
+			    <li><a href="happy-tips/" title="Happy Tips">Happy Tips</a></li>
+			    <li><a href="#" title="Events">Events</a></li>			
+			    <li><a href="bathroom-moments" title="Bathroom Secrets">Bathroom Moments</a></li>
+			    <li><a href="#" title="News">News</a></li>
+			</ul>
+		    </div>
+		</div>
+		<div class="top-highlight"></div>
+	    </div>
+
+	    <div class="content-container">
+			<div class="inner-wrapper">
+				<div class="row-fluid top-space">
+				<div class="span12">
+				    
+				    <div id="happiness-carousel" class="carousel slide">
+				    	<div class="carousel-inner">
+				    		<div class="active item">
+								<a href="#"><img src="http://www.reece.com.au/assets/pages/thumbnails/I-love-my-bathroom-hero-banner.jpg" alt="title" class="hidden-phone" />
+								<img src="http://www.reece.com.au/assets/pages/thumbnails/I-Love-My-Bathroom-Mobile-Banner.jpg" alt="title" class="visible-phone" /></a>
+				    		</div>
+				    		<div class="item">
+								<a href="#"><img src="http://www.reece.com.au/assets/pages/thumbnails/The-Block-Skyhigh-Hero-Banner.jpg" alt="title" class="hidden-phone" />
+								<img src="http://www.reece.com.au/assets/pages/thumbnails/The-Block-Skyhigh-Mobile-Banner.jpg" alt="title" class="visible-phone" /></a>
+				    		</div>
+				    	</div>
+				    	<!-- Carousel nav -->
+						<a class="carousel-control left" href="#happiness-carousel" data-slide="prev">&lsaquo;</a>
+						<a class="carousel-control right" href="#happiness-carousel" data-slide="next">&rsaquo;</a>
+				    </div>
+				    
+				</div>
+			    </div>
+			    <div id="landingTiles">
+				    <div class="row-fluid hidden-tablet">
+						<div class="span3 bottom-space">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/discover-620x275.jpg" alt="title" />
+						    	<div class="caption"><h4>Discover Products</h4><hr><p>Browse Australia's largest range of products to help you create the perfect bathroom</p></div>
+						    </a>
+						</div>
+						<div class="span3 bottom-space alt1">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/inspired-620x275.jpg" alt="title" />
+						    	<div class="caption"><h4>Be Inspired</h4><hr><p>Checkout the photo gallery for all the latest looks and hottest bathroom trends</p></div>
+							</a>
+						</div>
+						<div class="span3 bottom-space alt2">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/tips-620x275.jpg" alt="title" />
+						    	<div class="caption"><h4>Happy Tips</h4><hr><p>Simple solutions to help make your bathroom a happy place</p></div>
+							</a>
+						</div>
+						<div class="span3 bottom-space alt3">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/laufen-hz2.jpg" alt="title" />
+						    	<div class="caption"><h4>Bathroom Innovation</h4><hr><p>See the bathroom products of tomorrow with concepts by Australia's best designers</p></div>
+							</a>
+						</div>
+				    </div>
+				    
+				    <div class="row-fluid visible-tablet">
+						<div class="span6 bottom-space">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/discover-620x275.jpg" alt="title" />
+						    	<div class="caption"><h4>Discover Products</h4><hr><p>Browse over 1500 products designed to help you create the perfect bathroom</p></div>
+							</a>
+						</div>
+						<div class="span6 bottom-space alt1">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/inspired-620x275.jpg" alt="title" />
+						    	<div class="caption"><h4>Be Inspired</h4><hr><p>Checkout the photo gallery for all the latest looks and hottest bathroom trends</p></div>
+							</a>
+						</div>
+					</div>
+					<div class="row-fluid  hidden-desktop hidden-phone">
+						<div class="span6 bottom-space alt2">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/tips-620x275.jpg" alt="title" />
+						    	<div class="caption"><h4>Happy Tips</h4><hr><p>Simple solutions to help make your bathroom a happy place</p></div>
+							</a>
+						</div>
+						<div class="span6 bottom-space alt3">
+							<a href="#">
+						    	<img src="http://www.reece.com.au/assets/Uploads/laufen-hz2.jpg" alt="title" />
+						    	<div class="caption"><h4>Bathroom Innovation</h4><hr><p>See the bathroom products of tomorrow with concepts by Australia's best designers</p></div>
+							</a>
+						</div>
+				    </div>
+				    
+			    </div>
+			</div>
+		</div>
+
+
+			<?php 
+			   	include_once($serverBase."/includes/foot/foot-generic.php");
+			?>		
+
+		</div>
+
+	   	<?php
+	   		$mobileNavActiveMenu = array(
+	   			"activeMenu" => "Bathrooms",
+	   			"activeMenuItem" => "Happiness Zone"
+	   		);
+		   	include_once($serverBase."/includes/nav/nav-mobile.php");
+		?>	
+
+		<?php 
+		  	$jsScriptPaths = array(
+				"/assets/js/jquery.mobile.custom.min.js",		  		
+		  		"/assets/js/jquery.lazyload.min.js",
+		  		"/assets/js/bootstrap-transition.js", 
+		  		"/assets/js/bootstrap-carousel.js",
+		  		"/assets/js/bootstrap-collapse.js",
+		  		"/assets/js/reece-ocnav.js",
+		  		"/assets/js/reece-carousel.js",
+		  		"/assets/js/happiness-zone.js"
+		  	); 
+		  	include_once($serverBase."/includes/foot/scripts.php");
+		?>	
+
+	</body>
+</html>

b311cdd11072b7500d725171082db82fb8127269
Anton Punith <antonpunith@gmail.com>
2013-11-04 20:07:15 +1100

    adding page for onsite

---
+++ onsite/index.php
@@ -0,0 +1,175 @@
+
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/variables/variables.php"); ?>
+
+	<?php 
+		$cssScriptPaths = [
+			"/assets/css/reece-trade-outlet.css",
+			"/assets/css/reece-onsite.css"
+		]; 
+	   	include_once($serverBase."/includes/head/head-generic.php");
+	?>		
+
+    <body class="no-touch bathroom-moments account-details-summary withSubNav irrigationPage">
+		<div class="ocmain-wrapper">
+			<div class="navbar navbar-fixed-top topNav">
+			<?php 
+				$genericNavActiveMenuItem = "Onsite";
+			   	include_once($serverBase."/includes/nav/nav-generic.php");
+			?>				    
+
+			<?php 
+			   	include_once($serverBase."/includes/nav/nav-subnavbar.php");
+			?>	
+			</div>
+	    <div class="content-container">
+			<div class="inner-wrapper">
+
+				<div class="article-banner-trade page-branding box-brand clearfix">
+					<div class="banner-branding-block page-branding box-brand pull-left">
+						<h3 class="banner-branding-title">onsite</h3>
+					</div>
+					<p class="article-banner-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mollis id est a facilisis. Praesent pellentesque erat eget enim convallis porta.</p>
+					<p class="article-banner-description-trimmed">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+
+			    <article class="article-summary-main row-fluid">
+			    	<div class="span8">
+			    		<img src="/assets/images/trade_2013/trade_article_sample.jpg" alt="Trade sample" />
+			    	</div>
+			    	<div class="span4">
+    					<h3 class="headline article-headline">The smart way to keep your business moving</h3>
+    					<hr>
+    					<small class="page-branding text-brand">1 June 2013</small>
+    					<p>
+							A big part of being a licensed plumber is certifying the quality of work you do. That’s why warranties are so important. Boria de volorio omnis as periae pra que eserepe rumqui officiasit pos dicil explant ommolorem harchicia qui nonsequia plignam sim esequib usaped et veliquam fuga. Ut seria quis dunoptatum sit dollabo. Ut faccus.
+						</p>
+						<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+					</div>			    		
+				</article>				
+
+				<div data-resize-group="gateway" class="article-gateway row-fluid">
+					<article data-resize="height" class="article span3">
+						<a href="#"><img src="http://placehold.it/480x250" alt="Trade Sample Mini" /></a>
+						<h3>Hot water decisions guide</h3>
+						<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+					</article>
+
+					<article data-resize="height" class="article span3">
+						<a href="#"><img src="http://placehold.it/480x250" alt="Trade Sample Mini" /></a>
+    					<h3>Rothenberger World Series Competition</h3>
+						<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+					</article>
+
+					<article data-resize="height" class="article span3">
+						<a href="#"><img src="http://placehold.it/480x250" alt="Trade Sample Mini" /></a>
+	    				<h3>Reece Footy Tipping Competition</h3>
+						<a class="read-more page-branding text-brand" href="#">Log in to your footy tipping<i class="icon-arrow-right"></i></a>
+					</article>	
+
+					<article data-resize="height" class="article span3">
+						<a href="#"><img src="http://placehold.it/480x250" alt="Trade Sample Mini" /></a>
+    					<h3>Discover Thermann Hot Water Units</h3>
+						<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+					</article>							
+				</div>	
+
+			</div>
+	    </div>	
+	    
+	    <div class="recent-stories">
+	    	<div class="content-container">
+				<div class="inner-wrapper">
+					<h3>Recent Stories</h3>
+					<div class="article-summary-preview row-fluid">
+
+						<article class="article span2">
+							<a class="article-image" href="#"><img src="/assets/images/trade_2013/trade_article_sample_small_04.jpg" alt="Trade Sample Mini" /></a>
+		    				<div class="article-body">
+			    				<small class="page-branding text-brand">1 June 2013</small>
+			    				<h3>The peace of mind in having the right connections</h3>
+
+								<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+							</div>
+						</article>
+
+						<article class="article span2"> 
+							<a class="article-image" href="#"><img src="/assets/images/trade_2013/trade_article_sample_small_05.jpg" alt="Trade Sample Mini" /></a>
+		    				<div class="article-body">
+			    				<small class="page-branding text-brand">1 July 2013</small>
+			    				<h3>Rothenberger World Series Competition</h3>
+								<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+							</div>
+						</article>
+
+						<article class="article span2">
+							<a class="article-image" href="#"><img src="/assets/images/trade_2013/trade_article_sample_small_06.jpg" alt="Trade Sample Mini" /></a>
+		    				<div class="article-body">
+			    				<small class="page-branding text-brand">11 September 2013</small>
+			    				<h3>Where did my warranty go?</h3>
+								<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+							</div>
+						</article>
+						<article class="article span2">
+							<a class="article-image" href="#"><img src="/assets/images/trade_2013/trade_article_sample_small_04.jpg" alt="Trade Sample Mini" /></a>
+		    				<div class="article-body">
+			    				<small class="page-branding text-brand">1 June 2013</small>
+			    				<h3>The peace of mind in having the right connections</h3>
+								<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+							</div>
+						</article>
+
+						<article class="article span2">
+							<a class="article-image" href="#"><img src="/assets/images/trade_2013/trade_article_sample_small_05.jpg" alt="Trade Sample Mini" /></a>
+		    				<div class="article-body">
+			    				<small class="page-branding text-brand">1 July 2013</small>
+			    				<h3>Rothenberger World Series Competition</h3>
+								<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+							</div>
+						</article>
+
+						<article class="article span2">
+							<a class="article-image" href="#"><img src="/assets/images/trade_2013/trade_article_sample_small_06.jpg" alt="Trade Sample Mini" /></a>
+		    				<div class="article-body">
+			    				<small class="page-branding text-brand">11 September 2013</small>
+			    				<h3>Where did my warranty go?</h3>
+								<a class="read-more page-branding text-brand" href="#">Read More<i class="icon-arrow-right"></i></a>
+							</div>
+						</article>							
+					</div>
+				</div>	
+
+			    <!-- END CONTENT -->
+
+			</div>
+	    </div>
+
+	    <?php 
+			include_once($serverBase."/includes/foot/foot-generic.php");
+		?>	
+
+	</div>
+
+   	<?php
+   		$mobileNavActiveMenu = array(
+   			"activeMenu" => "Plumbing"
+   		);
+	   	include_once($serverBase."/includes/nav/nav-mobile.php");
+	?>	
+
+	<?php 
+		$jsScriptPaths = [
+			"/assets/js/jquery.lazyload.min.js", 
+			"/assets/js/mediaelement-and-player.min.js",
+			"/assets/js/bootstrap-collapse.js",
+			"/assets/js/bootstrap-dropdown.js",
+			"/assets/js/bootstrap-transition.js",
+			"/assets/js/jquery.mobile.custom.min.js",
+			"/assets/js/reece-ocnav.js",
+			"/assets/js/reece-match-height.js"
+
+		]; 
+	   	include_once($serverBase."/includes/foot/scripts.php");
+	?>
+
+    </body>
+</html>

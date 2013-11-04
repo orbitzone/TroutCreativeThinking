66ff1e20de2627265e5a4237590a3c563a77de53
Anton Punith <antonpunith@gmail.com>
2013-11-04 11:15:59 +1100

    updating nav, real content for home page

--- navigation/new-nav.php
+++ navigation/new-nav.php
@@ -4,7 +4,8 @@
 	<?php 
 		$cssScriptPaths = [
 			"/assets/css/reece-trade-outlet.css",
-			"/assets/css/reece-nav-updated.css"
+			"/assets/css/reece-nav-updated.css",
+      "/assets/css/reece-home.css"
 		]; 
 	   	include_once($serverBase."/includes/head/head-generic.php");
 	?>		
@@ -19,29 +20,190 @@
 
 	    <div class="content-container">
 			<div class="inner-wrapper">
-<br><br><br><br><br><br><br><br>        
-					<div class="row-fluid">
-						<h2>Lorem ipsum dolor sit</h2>
-						<div>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat pulvinar neque, quis congue turpis. Suspendisse sit amet tristique leo. Quisque auctor ac elit vel adipiscing. Aliquam erat volutpat. Donec eu dolor id odio malesuada semper sed vitae magna. Mauris convallis ac eros et ultricies. Vivamus eu odio eu nulla tincidunt rhoncus. Vivamus faucibus lectus id turpis laoreet, et pretium odio tincidunt. Duis ac elit sagittis, malesuada magna eget, lacinia ante.
-							</p>
-							<p>
-								Vivamus posuere erat in mollis feugiat. Nullam facilisis lacus vel massa aliquam, id eleifend diam posuere. Praesent sollicitudin sapien nibh, ut commodo nibh scelerisque sit amet. Vivamus bibendum mauris eget nulla tempor, sit amet ornare lorem vulputate. Phasellus tincidunt, dolor id molestie luctus, nunc lorem dignissim nisi, ut tempor turpis tellus quis nisl. Curabitur accumsan et enim sit amet luctus. Ut condimentum lobortis vestibulum. Maecenas in interdum dolor, a feugiat felis. Cras eu laoreet tortor, non cursus ante.
-							</p>
-							<p>
-								Fusce porttitor erat et eros feugiat, ut fermentum ante eleifend. Nullam commodo nisi nec velit laoreet porta. Aliquam erat dolor, porta non lectus nec, dapibus malesuada turpis. Vivamus placerat ut urna at viverra. Suspendisse fringilla metus vel mi fermentum viverra. Donec aliquet urna eros, congue molestie purus mollis non. Aliquam luctus sit amet eros quis sollicitudin. Vestibulum quis quam nisi. Suspendisse dictum rhoncus augue, non aliquet massa elementum et. Suspendisse sed vestibulum lorem, nec rhoncus felis. Fusce vitae sapien ullamcorper, fermentum eros at, faucibus mi. Nulla quis lectus felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam ut nulla ac sapien dignissim fringilla id a urna. Fusce ultrices sit amet turpis in fringilla.
-							</p>
-							<p>
-								Praesent luctus pretium placerat. Aenean quis augue luctus nisi tristique tristique vitae a ipsum. Proin est arcu, dapibus sed pellentesque sit amet, aliquam non dolor. Nam non ligula quis magna aliquet tincidunt. Morbi dignissim, nulla sed vestibulum fringilla, nunc massa luctus urna, et porttitor eros nulla feugiat mauris. Aenean vitae velit sed eros aliquet malesuada. Nulla non blandit elit. Proin in mauris in augue volutpat feugiat. Ut in turpis at ante pellentesque lobortis sit amet non augue. Etiam vehicula pretium neque, tincidunt semper ipsum accumsan eu. Morbi commodo posuere mauris, a porta magna vehicula a. Curabitur eget neque dolor. Duis dictum sit amet est quis venenatis. Aliquam neque nibh, placerat sit amet consequat in, tristique in mauris.
-							</p>
-							<p>
-								Suspendisse potenti. Praesent tellus orci, euismod id sem in, molestie lobortis erat. Proin sagittis eget massa nec auctor. Vivamus augue ipsum, ultrices pharetra blandit molestie, aliquam in tellus. In at fringilla metus. Sed aliquet venenatis diam et pulvinar. Vivamus lobortis euismod tempor. Phasellus nec aliquet enim. Nunc ultricies erat ante, non scelerisque odio accumsan nec.
-							</p>
-						</div>
 
-					</div>
-			</div>
+<div id="headCarousel" class="carousel  slide hidden-phone" data-bs-carousel="carousel" data-bs-carousel-interval="0">
+	<!-- Carousel items -->
+	<div class="carousel-inner">
+        
+				<div class="item active">
+					<a href="/bathroom-sale">
+						<img src="http://www.reece.com.au/assets/pages/thumbnails/bathroom-sale-reece.jpg" alt="Spring Sale" class="visible-desktop">
+                        
+							<img src="http://www.reece.com.au/assets/pages/thumbnails/bathroom-sale-reece2.jpg" alt="Spring Sale" class="hidden-desktop">
+                        
+					</a>
+				</div>
+        
+				<div class="item ">
+					<a href="/diydisasters">
+						<img src="http://www.reece.com.au/assets/pages/thumbnails//DIY-Disasters-Competition.jpg" alt="DIY Disasters" class="visible-desktop">
+                        
+							<img src="http://www.reece.com.au/assets/pages/thumbnails//DIY-Disasters-Competition2.jpg" alt="DIY Disasters" class="hidden-desktop">
+                        
+					</a>
+				</div>
+        
+	</div>
+    
+		<a class="carousel-control left background--light" href="/#headCarousel" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right background--light" href="/#headCarousel" data-slide="next">&rsaquo;</a>
+    
+</div>
+
+
+
+<div class="content-container hidden-phone">
+    <div class="inner-wrapper">
+        <div class="row-fluid features">
+            
+            
+
+            
+            <div class="span3 bathrooms">        
+    
+    <div class="feature-item feature-bathrooms tile-item">
+        <a href="/bathrooms">
+            <img src="http://www.reece.com.au/assets/pages/thumbnails/feature-bathrooms.jpg" alt="feature bathrooms">
+        </a>
+        <div class="caption">
+            <a href="/bathrooms"><h4>Bathrooms</h4></a>
+        </div>
+        <a href="/bathrooms" class="primary-link" title="Bathrooms"></a>
+    </div>
+    
+</div>
+            
+
+            
+
+            
+            <div class="span3 plumbing">        
+    
+    <div class="feature-item feature-plumbing tile-item">
+        <a href="http://reece.com.au/plumbing">
+            <img src="http://www.reece.com.au/assets/pages/thumbnails/feature-plumbing.jpg" alt="feature plumbing">
+        </a>
+        <div class="caption">
+            <a href="http://reece.com.au/plumbing"><h4>Plumbing</h4></a>
+        </div>
+        <a href="http://reece.com.au/plumbing" class="primary-link" title="Plumbing"></a>
+    </div>
+    
+</div>
+            
+
+            
+
+            
+            <div class="span3 specialised">        
+        
+    <div class="row-fluid">
+        
+        <div class="span6 bottom-space">
+            <div class="feature-item feature-irrigation tile-item ">
+                <a href="http://reece.com.au/irrigation">
+                    <img src="http://www.reece.com.au/assets/pages/thumbnails/feature-irrigation.jpg" alt="feature irrigation">
+                </a>
+                <div class="caption">
+                    <a href="http://reece.com.au/irrigation">
+                        <h4>Irrigation</h4>
+                    </a>
+                </div>
+                <a href="http://reece.com.au/irrigation" class="primary-link" title="Irrigation"></a>
+            </div>
+        </div>  
+        
+        
+        <div class="span6 bottom-space">
+            <div class="feature-item feature-onsite tile-item last">
+                <a href="http://reece.com.au/onsite">
+                    <img src="http://www.reece.com.au/assets/pages/thumbnails/feature-onsite.jpg" alt="feature onsite">
+                </a>
+                <div class="caption">
+                    <a href="http://reece.com.au/onsite">
+                        <h4>Onsite</h4>
+                    </a>
+                </div>
+                <a href="http://reece.com.au/onsite" class="primary-link" title="Onsite"></a>
+            </div>
+        </div>  
+        
+    </div>
+    <div class="row-fluid">
+        
+        
+        <div class="span6 bottom-space">
+            <div class="feature-item feature-civil tile-item ">
+                <a href="http://reece.com.au/civil">
+                    <img src="http://www.reece.com.au/assets/pages/thumbnails/feature-civil2.jpg" alt="feature civil2">
+                </a>
+                <div class="caption">
+                    <a href="http://reece.com.au/civil">
+                        <h4>Civil</h4>
+                    </a>
+                </div>
+                <a href="http://reece.com.au/civil" class="primary-link" title="Civil"></a>
+            </div>
+        </div>  
+        
+        
+        <div class="span6 bottom-space">
+            <div class="feature-item feature-hvacr tile-item last">
+                <a href="http://reece.com.au/hvacr">
+                    <img src="http://www.reece.com.au/assets/pages/thumbnails/feature-hvacr.jpg" alt="feature hvacr">
+                </a>
+                <div class="caption">
+                    <a href="http://reece.com.au/hvacr">
+                        <h4>HVAC-R</h4>
+                    </a>
+                </div>
+                <a href="http://reece.com.au/hvacr" class="primary-link" title="HVAC-R"></a>
+            </div>
+        </div>  
+        
+    </div>
+    <div class="row-fluid">
+        
+        
+    </div>
+    
+</div>
+            
+
+            
+
+            
+            <div class="span3 bottom-space">    
+    <div class="feature-item tile-item feature-storefinder last">
+        <img alt="title" src="http://www.reece.com.au/assets/pages/thumbnails/feature-storefinder.png">
+        <div class="form-wrapper">
+            <h4>Store Finder</h4>
+            <form method="post" action="http://www.reece.com.au/storefinder" class="storefinderform">
+                <input type="hidden" name="point" value="">
+                <select name="homepageStoreType" class="span8 hidden-phone">
+                    <option value="">All store types</option>
+                    <option value="BS">Bathroom Showroom</option>
+                    <option value="P">Plumbing Centre</option>
+                    <option value="I">Irrigation</option>
+                    <option value="H">HVAC-R</option>
+                    <option value="R">Refrigeration</option>
+                    <option value="C">Civil</option>
+                    <option value="O">Onsite</option>
+                </select>
+
+                <span class="hidden-phone input-append">
+                    <input type="text" class="span8" placeholder="Suburb/Postcode" name="origin">
+                    <input type="submit" class="btn span3" value="GO">
+                </span>
+            </form>
+        </div>
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+		</div>
 	    </div>	
 	    
     <?php 

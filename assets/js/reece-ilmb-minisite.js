2fcf0bd8280a0751dad243bbba8e289c22720a1d
Anton Punith <antonpunith@gmail.com>
2014-01-15 16:03:41 +1100

    updating the design for mobile and tablets

--- assets/js/reece-ilmb-minisite.js
+++ assets/js/reece-ilmb-minisite.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	    overflow: 'hidden'
 	});
 	$target.fadeOut(function(){
-	    $target.load($link.attr('href')+'?load=1', function(){
+	    $target.load($link.attr('href'), function(){
 		$('.device-sub-menu').collapse('hide');
 		$target.fadeIn(function(){
 		    $wrapper.animate({

b9e43ae7fbf69f976839731ec99854c1bddadfa9
Elbone <e@elbone.com.au>
2014-08-20 10:39:38 +1000

    Further fixes to History

--- aboutus/history.php
+++ aboutus/history.php
@@ -80,83 +80,79 @@
 			"/assets/js/jquery.mobile.custom.min.js",
 			"/assets/js/bootstrap-scrollspy.js",
 			"/assets/js/reece-ocnav.js",
-			//"/assets/js/skrollr.min.js",
+			"/assets/js/skrollr.min.js",
 			"/assets/js/reece-hidesubnavbar.js",
 			"/assets/js/reece-history.js"
 		];
 		include_once($serverBase."/includes/foot/scripts.php");
 	?>
 	<script type="text/javascript">
-		// Check for support
-		var Timeline = function() {
-			window.TT = this;
-			TT.hasTouch = Modernizr.touch;
-			TT.smallScreen = $(window).width() < 800;
-			TT.minHeight = 800;
-			TT.maxHeight = 1050;
-			TT.container = '#timeline';
-			TT.html = '';
-			TT.ajax = {
-				full: 'history/ajax-history-full.php',
-				small: 'history/ajax-history-images.php'
-			};
-			TT.skrollrConfig = {
-				smoothScrolling : true,
-				smoothScrollingDuration: 2000,
-				mobileCheck : function() {
-					return false; // disable for mobile
-				}
-        	};
-		};
-		Timeline.prototype.SkrollrSetup = function () {
-			if (TT.smallScreen || TT.hasTouch) return;
-			TT.skrollr = skrollr.init(TT.skrollrConfig).refresh();
-		};
-		Timeline.prototype.SetHeights = function () {
-			var win_height = $(window).height()*0.8;
-			if (win_height > TT.minHeight && win_height < TT.maxHeight) {
-				$(TT.container).find('section').css('min-height', win_height);
-			}
-		};
-		Timeline.prototype.GetHTML = function() {
-			if (TT.hasTouch || TT.smallScreen) {
-				$.get(TT.ajax.small)
-					.done(
-							function (data, textStatus, jqXHR) {
-								$(TT.container).empty().html(data);
-								// $('body').css('overflow', 'auto');
-								// console.log(skrollr);
-						}
-					);
-			}
-			else {
-				$.get(TT.ajax.full)
-					.done(
-						function (data, textStatus, jqXHR) {
-							$(TT.container).empty().html(data);
-						}
-					)
-					.then(
-						function() {
-							TT.SetHeights();
-						}
-					)
-					.then(
-						function() {
-							//TT.SkrollrSetup();
-						}
-					);
-			}
-		};
-
+		// Keep this here
 		$(document).on('ready', function() {
 
+			var Timeline = function() {
+				window.TT = this;
+				TT.hasTouch = Modernizr.touch;
+				TT.smallScreen = $(window).width() < 1000; // 1000px == min-width of large slides
+				TT.minHeight = 800;
+				TT.maxHeight = 1050;
+				TT.container = '#timeline';
+				TT.ajax = {
+					full: 'history/ajax-history-full.php',
+					small: 'history/ajax-history-images.php'
+				};
+				TT.skrollrConfig = {
+					smoothScrolling : true,
+					smoothScrollingDuration: 2000
+					// mobileCheck : function() {
+					// 	return false; // disable for mobile
+					// }
+				};
+
+				// Init
+				TT.GetHTML();
+			};
+			Timeline.prototype.SkrollrSetup = function () {
+				if (TT.smallScreen || TT.hasTouch) return;
+				TT.skrollr = skrollr.init(TT.skrollrConfig);
+			};
+			Timeline.prototype.SetHeights = function () {
+				var win_height = $(window).height()*0.8;
+				if (win_height > TT.minHeight && win_height < TT.maxHeight) {
+					$(TT.container).find('section').css('min-height', win_height);
+				}
+			};
+			Timeline.prototype.GetHTML = function() {
+				if (TT.hasTouch || TT.smallScreen) {
+					$.get(TT.ajax.small)
+						.done(
+							function (data, textStatus, jqXHR) {
+								$(TT.container).empty().html(data);
+							}
+						);
+				}
+				else {
+					$.get(TT.ajax.full)
+						.done(
+							function (data, textStatus, jqXHR) {
+								$(TT.container).empty().html(data);
+							}
+						)
+						.then(
+							function() {
+								TT.SetHeights();
+							}
+						)
+						.then(
+							function() {
+								TT.SkrollrSetup();
+							}
+						);
+				}
+			};
 			// Init timeline object
 			var timeline = new Timeline();
-			timeline.GetHTML();
-
 		});
-
 	</script>
 
 </body>

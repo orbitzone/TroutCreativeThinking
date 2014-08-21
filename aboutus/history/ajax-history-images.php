88ccda1b69d7a6b649d7bc082bf91f4a99876acc
Elbone <e@elbone.com.au>
2014-08-21 11:03:21 +1000

    Fixed fonts for mobile

--- aboutus/history/ajax-history-images.php
+++ aboutus/history/ajax-history-images.php
@@ -7,7 +7,7 @@
 		echo '<section id="slide'.$year.'" class="'.$attr[0].' small">';
 		echo '<div class="header">';
 		echo '<h2 class="year">'.$y.'</h2>';
-		echo '<p class="yearDescription">'.$attr[1].'.</p>';
+		echo '<p class="yearDescription">'.$attr[1].'</p>';
 		echo '</div>';
 		echo file_exists($img) ? '<img src="history/'.$img.'"/>' : '';
 		echo '</section>';

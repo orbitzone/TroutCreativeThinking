37d90634a4e96e3bd4807e7404c1e053bf8b1321
Joey Kao <joey@trout.com.au>
2014-09-09 16:13:10 +1000

    initial commit of The Spring Bathroom 2014 Splash page

---
+++ bathroom/spring2014/index.php
@@ -0,0 +1,158 @@
+
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/variables/variables.php"); ?>
+
+	<?php 
+		$cssScriptPaths = [
+			"/assets/css/reece-products.css",
+			"/assets/css/reece-bathrooms.css",
+			"/assets/css/reece-spring2014.css"			
+		]; 
+	   	include_once($serverBase."/includes/head/head-generic.php");
+	?>		
+
+    <body class="withHappinessSubNav">
+		<div class="ocmain-wrapper">
+  <div class="navbar navbar-fixed-top topNav">
+    <?php
+					$genericNavActiveMenuItem = "Bathrooms";
+				   	include_once($serverBase."/includes/nav/nav-generic.php");
+				?>
+  </div>
+    <?php include_once($serverBase."/includes/nav/nav-happiness.php"); ?>
+		    <div class="content-container">
+				<div class="inner-wrapper">
+
+
+
+				    <!-- CONTENT FOR DESKTOP -->
+
+					<section class="products-content">
+						<img src="http://placehold.it/1600x459" alt="Laufen">
+						<p class="product-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</section>
+
+					<div class="sort-filter clearfix">
+						<div class="dropdown pull-right">
+							<a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+								Sort by
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+								<li role="filter"><a href="#">Price</a></li>
+								<li role="filter"><a href="#">Most Popular</a></li>
+								<li role="filter"><a href="#">Newest Releases</a></li>
+								<li role="filter"><a href="#">Our Picks</a></li>									
+							</ul>
+						</div>
+						<hr>
+				    </div>
+
+			    	<h3>Basins</h3>
+				    	
+			    	<div class="products-list">
+					    <div class="row-fluid">
+
+					    	<div class="product-block span5 offset1">
+					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
+								<div class="product-info">
+									<h4>
+										<a class="product-title" href="#">Product</a>
+										<span class="product-desc">Freestanding bath</span>
+										<span class="product-price">$xxx.xx</span>
+									</h4>
+								</div>							    		
+					    	</div>
+					    	<div class="product-block span5">
+					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
+								<div class="product-info">
+									<h4>
+										<a class="product-title" href="#">Product</a>
+										<span class="product-desc">Freestanding bath</span>
+										<span class="product-price">$xxx.xx</span>
+									</h4>
+								</div>							    		
+					    	</div>					    							    							    	
+					    </div>
+					</div>									
+
+				    <hr>
+
+			    	<h3>Toilets</h3>
+				    	
+			    	<div class="products-list">
+					    <div class="row-fluid">
+					    	<div class="product-block span3">
+								<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
+								<div class="product-info">
+									<h4>
+										<a class="product-title" href="#">Product</a>
+										<span class="product-desc">Freestanding bath</span>
+										<span class="product-price">$xxx.xx</span>
+									</h4>
+								</div>
+					    	</div>
+					    	<div class="product-block span3">
+					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
+								<div class="product-info">
+									<h4>
+										<a class="product-title" href="#">Product</a>
+										<span class="product-desc">Freestanding bath</span>
+										<span class="product-price">$xxx.xx</span>
+									</h4>
+								</div>							    		
+					    	</div>
+					    	<div class="product-block span3">
+					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
+								<div class="product-info">
+									<h4>
+										<a class="product-title" href="#">Product</a>
+										<span class="product-desc">Freestanding bath</span>
+										<span class="product-price">$xxx.xx</span>
+									</h4>
+								</div>							    		
+					    	</div>
+					    	<div class="product-block span3">
+					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
+								<div class="product-info">
+									<h4>
+										<a class="product-title" href="#">Product</a>
+										<span class="product-desc">Freestanding bath</span>
+										<span class="product-price">$xxx.xx</span>
+									</h4>
+								</div>							    		
+					    	</div>					    							    							    	
+					    </div>
+					</div>	
+
+				</div>
+		    </div>
+
+			<?php 
+			   	include_once($serverBase."/includes/foot/foot-generic.php");
+			?>		
+
+		</div>		
+
+	   	<?php
+	   		$mobileNavActiveMenu = array(
+	   			"activeMenu" => "Bathrooms",
+	   			"activeMenuItem" => "Bathroom Moments"
+	   		);
+		   	include_once($serverBase."/includes/nav/nav-mobile.php");
+		?>	
+
+		<?php 
+			$jsScriptPaths = [
+				"/assets/js/jquery.mobile.custom.min.js",
+				"/assets/js/bootstrap-transition.js", 
+				"/assets/js/bootstrap-carousel.js",
+				"/assets/js/bootstrap-collapse.js",
+				"/assets/js/bootstrap-dropdown.js",
+				"/assets/js/reece-ocnav.js",
+				"/assets/js/reece-carousel.js"
+			]; 
+		   	include_once($serverBase."/includes/foot/scripts.php");
+		?>			
+
+    </body>
+</html>

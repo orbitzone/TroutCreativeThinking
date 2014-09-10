21df036e0c074240942b0191cc662edc3a8e085e
Joey Kao <joey@trout.com.au>
2014-09-10 19:25:14 +1000

    Basic structure of Spring splash page

--- bathroom/spring2014/index.php
+++ bathroom/spring2014/index.php
@@ -10,7 +10,7 @@
 	   	include_once($serverBase."/includes/head/head-generic.php");
 	?>		
 
-    <body class="spring2014 withHappinessSubNav">
+    <body class="3 withHappinessSubNav">
 		<div class="ocmain-wrapper">
   <div class="navbar navbar-fixed-top topNav">
     <?php
@@ -27,31 +27,152 @@
 
 
 				    	
-			    	<!--div class="products-list">
-					    <div class="row-fluid">
+			    	<section class="spring-header container">
+			    		<div class="row-fluid">
+					    	<div class="span12">
+					    		
 
-					    	<div class="product-block span5 offset1">
-					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
-								<div class="product-info">
-									<h4>
-										<a class="product-title" href="#">Product</a>
-										<span class="product-desc">Freestanding bath</span>
-										<span class="product-price">$xxx.xx</span>
-									</h4>
-								</div>							    		
+					    		<div class="header-wrap">
+
+					    			<div class="header-image">
+					    				<img src="/assets/images/spring2014/main-banner.jpg" />	
+					    			</div>
+					    			<div class="header-text">
+					    				<h1>The Spring Bathroom</h1>
+					    				<div class="launch-button"><a href="http://mag.reece.com.au/spring">
+					    					<span class="launch">Launch</span>
+					    					<span class="lookbook">Digital lookbook</span>
+					    				</a></div>
+
+					    			</div>
+
+					    		</div>
+
 					    	</div>
-					    	<div class="product-block span5">
-					    		<a href="#"><img src="/assets/images/products_2013/products-bathroom-winter-warmers-product-1.jpg" /></a>
-								<div class="product-info">
-									<h4>
-										<a class="product-title" href="#">Product</a>
-										<span class="product-desc">Freestanding bath</span>
-										<span class="product-price">$xxx.xx</span>
-									</h4>
-								</div>							    		
-					    	</div>					    							    							    	
 					    </div>
-					</div-->									
+
+					</section>			
+
+
+
+
+
+			    	<section class="spring-subscribe container">
+					    <div class="row-fluid">
+					    	<div class="span12">
+					    		<form id="subscribe-box">
+					    			<div class="subscribe-wrap">
+						    			<label>Subscribe for updates</label>
+
+						    			<input type="text" name="name" placeholder="Name">
+
+						    			<input type="email" name="email" placeholder="Email">
+
+						    			<input type="text" name="postcode" placeholder="Postcode">
+
+						    			<input type="submit" name="submit">
+						    		</div>
+
+
+					    		</form>				    		
+					    	</div>
+					    </div>
+					</section>	
+
+
+
+
+
+
+
+			    	<section class="spring-intro container">
+			    		<section class="row-fluid title">
+					    	<div class="span10 offset1">
+								<h2>Renovate. Refresh. Rejoice.</h2>
+					    	</div>
+					    </section>
+					    
+					    <section class="row-fluid intro">
+					    	<div class="span10 offset1">
+					    		<p>There is no better time to begin creating your new bathroom than in Spring. To help you along the journey to new bathroom happiness, we&rsquo;ve put together a collection of tips and inspiration.</p>
+					    	</div>
+					    </section>
+					</section>	
+
+
+			    	<section class="spring-items image-left container">
+					    <section class="row-fluid">
+					    	<div class="span10 offset1">
+
+					    		<div class="image-box">
+									<img src="/assets/images/spring2014/item1.jpg" alt="[The 7 steps to bathroom happiness]" />
+					    		</div>
+						    	<div class="text-box"><div class="text-wrap">
+						    		<h3>The 7 steps to bathroom happiness</h3>
+						    			<p>There is no better time to begin creating your new bathroom than in Spring. To help you along the journey to new bathroom happiness, we&rsquo;ve put together a collection of tips and inspiration.</p>
+						    			<a href="#" class="box-link1">Learn more</a>
+						    	</div></div>
+						    	<div class="clear"></div>
+					    	</div>
+					    </section>
+					</section>	
+
+			    	<section class="spring-items image-right container">
+					    <section class="row-fluid">
+					    	<div class="span10 offset1">
+
+					    		<div class="image-box">
+									<img src="/assets/images/spring2014/item2.jpg" alt="[Latest Trends]" />
+					    		</div>
+						    	<div class="text-box"><div class="text-wrap">
+						    		<h3>Latest Trends</h3>
+					    			<p>Economy of colour &amp; clean lines help minimise to create clarity and space.</p>
+					    			<a href="#" class="box-link2">Explore</a>
+						    	</div></div>
+						    	<div class="clear"></div>
+					    	</div>
+					    </section>
+					</section>	
+
+			    	<section class="spring-items image-left container">
+					    <section class="row-fluid">
+					    	<div class="span10 offset1">
+
+					    		<div class="image-box">
+									<img src="/assets/images/spring2014/item3.jpg" alt="Mini Makeover" />
+					    		</div>
+						    	<div class="text-box"><div class="text-wrap">
+									<h3>Mini Makeover</h3>
+					    			<p>This Spring, you can create a new look and feel for your bathroom without a major renovation. Our design experts show you how with these clever mini makeovers.</p>
+					    			<a href="#" class="box-link3">Refresh</a>
+						    	</div></div>
+						    	<div class="clear"></div>
+					    	</div>
+					    </section>
+					</section>	
+
+			    	<section class="spring-items image-right container">
+					    <section class="row-fluid">
+					    	<div class="span10 offset1">
+
+					    		<div class="image-box">
+									<img src="/assets/images/spring2014/item4.jpg" alt="[Easy updates]" />
+					    		</div>
+						    	<div class="text-box"><div class="text-wrap">
+						    		<h3>Easy updates</h3>
+					    			<p>Our collection of hand picked must-have products can inject new life in your bathroom this Spring. A little change can make a big impact. </p>
+					    			<a href="#" class="box-link4">Refresh</a>
+						    	</div></div>
+						    	<div class="clear"></div>
+					    	</div>
+					    </section>
+					</section>	
+
+
+
+
+
+
 
 
 				    	
@@ -82,10 +203,64 @@
 				"/assets/js/bootstrap-collapse.js",
 				"/assets/js/bootstrap-dropdown.js",
 				"/assets/js/reece-ocnav.js",
-				"/assets/js/reece-carousel.js"
+				"/assets/js/reece-carousel.js",
+				"/assets/js/bootstrap-modal.js"
 			]; 
 		   	include_once($serverBase."/includes/foot/scripts.php");
 		?>			
+
+
+
+
+ 
+
+		<div id="spring-modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		  <div class="top-section">
+
+
+		    
+
+		    <div class="main-modal-image">
+
+			
+		    <div class="popup-image"><img src="/assets/images/spring2014/popup-main.jpg" /></div>
+
+		    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><img class="close-image" src="/assets/images/spring2014/close-button.jpg" /></button>
+
+		    </div>
+
+
+
+		  </div>
+		  <div class="middle-section">
+		    <h2>The Spring Bathroom</h2>
+		    <p>
+			Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam quis risus eget urna mollis ornare vel eu leo.
+		    </p>
+		  </div>
+		  <div class="bottom-section">
+			<form>
+    			
+
+    			<input type="text" name="name" placeholder="Name">
+
+    			<input type="email" name="email" placeholder="Email">
+
+    			<input type="text" name="postcode" placeholder="Postcode"><br />
+
+    			<input type="submit" name="submit">
+
+
+    		</form>	
+		  </div>
+		</div>
+
+		<script type="text/javascript">
+			$(window).load(function(){
+		        $('#spring-modal').modal('show');
+		    });
+		</script>
+
 
     </body>
 </html>

ef2604eb80ced31bc54371725255b156d6c87af9
Lance Cooper <lwcooper@gmail.com>
2014-09-18 16:31:24 +1000

    Hitachi summer: adding content and adding all border styles

--- hitachi-summer/index.php
+++ hitachi-summer/index.php
@@ -21,28 +21,85 @@ include_once($serverBase."/includes/head/head-generic.php");
 		<div class="hitachi">
 			<div class="content-container">
 				<div class="inner-wrapper">
-					<div class="hitachi-header">
-						<h1 class="hitachi-header-title">Hitachi Summer Rewards</h1>
+					<div class="hitachi-title">
+						<h1>Hitachi Summer Rewards</h1>
 					</div>
-					<div class="hitachi-content">
-						<div class="hitachi-content-header">
-							<p>
-								<span class="hitachi-text-med">Buy 5 or more</span> 
-								<span class="hitachi-text-body">Hitachi Air Conditioners in October and November and be rewarded with a</span>
-								<span class="hitachi-text-med">voucher valued</span> 
-								<span class="hitachi-text-divider">
-									<span class="hitachi-text-sm">from</span> 
-									<span class="hitachi-text-lg">$200</span> 
-									<span class="hitachi-text-sm">to</span>
-									<span class="hitachi-text-lg">$1000</span>
-								</span>
-							</p>
-							<p><a href="#rewards">Discover Rewards</a></p>
+					<div class="hitachi-wrapper">
+						<div class="hitachi-header">
+							<div class="hitachi-header-content">
+								<p>
+									<span class="hitachi-text-med">Buy 5 or more</span> 
+									<span class="hitachi-text-body">Hitachi Air Conditioners in October and November and be rewarded with a</span>
+									<span class="hitachi-text-med">voucher valued</span> 
+									<span class="hitachi-text-divider">
+										<span class="hitachi-text-sm">from</span> 
+										<span class="hitachi-text-lg">$200</span> 
+										<span class="hitachi-text-sm">to</span>
+										<span class="hitachi-text-lg">$1000</span>
+									</span>
+								</p>
+							</div>
+						</div>
+
+						<div class="hitachi-tab">
+							<a href="#rewards" class="hitachi-tab-link">Discover Rewards</a>
+							<div class="hitachi-tab-background"></div>
+						</div>
+
+						<!-- <p class="hitachi-tab"><a href="#rewards">Discover Rewards</a></p>
+						<div class="hitachi-tab-background"></div> -->
+						<div class="hitachi-content">
+							<div class="hitachi-content-rewards" id="rewards">
+								<dl class="hitachi-reward-list">
+									<dt>5 - 9 units and receive a</dt>
+									<dd>$200 Coles voucher</dd>
+									<dt>10-19 units and receive a</dt>
+									<dd>$400 voucher </dd>
+									<dt>20-49 units and receive a</dt>
+									<dd>Visa debit card valued at $600</dd>
+									<dt>50+ units and receive a</dt>
+									<dd>$1000 travel voucher</dd>
+								</dl>
+								<p class="hitachi-legal">Reward period starts 1 October 2014 and ends 30 November 2014.</p>
+								<p class="hitachi-legal">Available to Reece account customers and Reece cash sale account customers.</p>
+								<p class="hitachi-terms"><a href="#">Terms &amp; conditions</a></p>
+							</div>
+							<div class="hitachi-content-terms" id="terms">
+								<p class="hitachi-content-back"><a href="#rewards">Back to Rewards</a></p>
+								<h2>2014 Reece Hitachi Summer Rewards Promotion</h2>
+								<h3>Terms and Conditions</h3>
+								<ol>
+									<li><strong>1.</strong> Information on this promotion form part of conditions of rewards.  Purchase of Hitachi units shall be deemed an acknowledgement and acceptance of these terms and conditions.</li>
+									<li><strong>2.</strong> Eligibility for rewards is only open to Australian residents who are Reece Account Customers or Reece Cash Account Customers during the rewards period. Employees and immediate families of the Promoter, Reece and associated companies and agencies are ineligible to claim rewards. </li>
+									<li><strong>3.</strong> The Rewards program commences on 1st October 2014 at 7.00am (AEST) and closes on 30th November 2014 at 11.59pm (Australian Eastern Standard Time).</li>
+									<li><strong>4.</strong> To be eligible for rewards participants must purchase Hitachi Wall Mounted, Cassette, Ducted or Multi Head Air Conditioners during the reward period. </li>
+									<li><strong>5.</strong> Each Reece account or Purchaser representing that Reece account warrants that they have the authority to grant Reece permission to use the information provided as contemplated by these terms and conditions.</li>
+									<li><strong>6.</strong> The rewards will be determined on or about the 10th of December by Reece's Marketing Manager. Companies eligible for rewards will be notified by telephone by their branch manager, and his or her name may be published in the February 2015 edition of Outlet newsletter.</li>
+									<li><strong>7.</strong> The rewards are based on the following Purchasing Criteria for October and November 2014 for the Promotion.
+										<h3>For Reece Account or Reece Cash Sales Customers.</h3>
+										<p>Purchase and Invoicing of:</p>
+										<ul>
+											<li><strong>5 – 9</strong> Hitachi Wall Mounted, Cassette, Ducted or Multi units and receive a $200.00 dollar Coles Voucher.</li>
+											<li><strong>10 – 19</strong>  Hitachi Wall Mounted, Cassette, Ducted or Multi units and receive a $400.00 dollar Coles Voucher.</li>
+											<li><strong>20 – 49</strong>  Hitachi Wall Mounted, Cassette, Ducted or Multi units and receive a $600.00 dollar Visa Debit Card.</li>
+											<li><strong>50 +</strong>  Hitachi Wall Mounted, Cassette, Ducted or Multi units and receive a $1,000.00 dollar Travel Voucher.</li>
+											<li><strong>*</strong> Credits during the reward period of the above units will be taken into account when determining the final total purchased quantities.</li>
+											<li><strong>*</strong> Rewards are not exchangeable or interchangeable for other rewards, cash or products.</li>
+										</ul>
+									</li>
+									<li><strong>8.</strong> Except for any liability that cannot be excluded by law, the Promoter will not be liable for any failure to provide rewards where such a failure arises out of any technical difficulties or equipment malfunction (whether or not under the Promoter's control).</li>
+									<li><strong>9.</strong> The Promoter shall not be liable for any loss or damage whatsoever suffered, including but not limited to direct or consequential loss, or personal injury suffered or sustained in connection with the rewards.</li>
+									<li><strong>10.</strong> The Promoter shall not be liable for any loss or damage whatsoever which is suffered (including but not limited to indirect or consequential loss) or for personal injury suffered or sustained, as a result of accepting the reward, except for any liability which cannot be excluded by law. The Promoter accepts no responsibility for any tax implications (e.g. Stamp Duty) that may arise from the rewards.</li>
+									<li><strong>11.</strong> For this competition winners must be aged over 18 years of age.</li>
+									<li><strong>12.</strong> The Promoter is Reece Pty Ltd, ACN 008 576 903, ABN 96 008 576 903, 118 Burwood Highway, Burwood, Victoria 3125, alternatively by telephone on 03 9274 0000.</li>
+								</ul>
+								<p class="hitachi-content-back"><a href="#rewards">Back to Rewards</a></p>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<p class="hitachi-terms"><a href="#">Terms &amp; conditions</a></p>
+			
 		</div>
 		<?php 
 		include_once($serverBase."/includes/foot/foot-generic.php");

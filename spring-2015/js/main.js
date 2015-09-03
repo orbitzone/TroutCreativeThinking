d7cf2dbb5e5249284d45fbd3e8c90b1e254ab4e1
Diego Pelaez <diego@trout.com.au>
2015-09-03 12:42:07 +1000

    Updated images. Added youtube iframe_api call and fixed video position on Calm.

--- spring-2015/js/main.js
+++ spring-2015/js/main.js
@@ -1,4 +1,5 @@
-var moodboard = {}
+var moodboard = {};
+var hotspots = {};
 var homePage = {};
 var loveFamily = {};
 var loveLuxury = {};
@@ -8,8 +9,7 @@ var bathroomsWeLove = {};
 var bathroomDestinationMakeovers = {};
 var becJudd = {};
 var mrJasonGrant = {};
-var bathroomsWeLove = {};
-
+var player = {};
 var isMobile = (function(a){return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
 $.fn.imagesLoaded = function () {
@@ -35,7 +35,52 @@ $.fn.imagesLoaded = function () {
     return $.when.apply($,dfds);
 
 };
+
+
 jQuery(function($){
+	player = {
+		obj:"",
+		init: function(container, videoId){
+			if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+        window.onYouTubeIframeAPIReady = function() {
+          player.loadPlayer(container, videoId);
+        };
+        //This code loads the IFrame Player API code asynchronously.
+				var tag = document.createElement('script');
+
+				tag.src = "https://www.youtube.com/iframe_api";
+				var firstScriptTag = document.getElementsByTagName('script')[0];
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      } else {
+        player.loadPlayer(container, videoId);
+      }
+		},
+		loadPlayer: function(container, videoId){
+		 player.obj = new YT.Player(container, {
+        videoId: videoId,
+        width: 356,
+        height: 200,
+        playerVars: {
+          autoplay: 0,
+          /*controls: 0,
+          modestbranding: 1,
+          rel: 0,
+          showInfo: 0*/
+        },
+        events: {
+        	'onReady': function(event){
+        		
+        	}
+        }
+      });
+		},
+		play: function(){
+			player.obj.playVideo();
+		},
+		stop: function(){
+		  player.obj.stopVideo();      
+		}
+	};
 	shareBoard = {
 		init: function(){
 			$('body .ocmain-wrapper').append('<div id="share-hex" class="share-hex">'
@@ -204,6 +249,34 @@ jQuery(function($){
 			}
 		}
 	};
+	hotsposts = {
+		init: function(){
+			$('#bathrooms-for-inspiration .hotspot').on('click',function(){
+				hotsposts.showData(this);
+			});
+		},
+		showData: function(obj){
+			var spot = $(obj).data("spot");
+			if($(obj).hasClass('active')){
+				$(obj).removeClass('active');
+				$('.hotspot-data.spot-'+spot).fadeOut();
+			}else{
+				$(obj).parent().find('.hotspot').removeClass('active');
+				$('.hotspot-data').fadeOut();
+				$(obj).addClass('active');
+				
+				var top = $(obj).position().top + $(obj).outerHeight()+10;
+				var left = $(obj).position().left - ($('.hotspot-data.spot-'+spot).outerWidth()/2) + ($(obj).outerHeight()/2);
+				$('.hotspot-data.spot-'+spot).css(
+				{
+					top: top,
+					left: left
+				})
+				$('.hotspot-data.spot-'+spot).fadeIn();
+				;
+			}
+		}
+	};
 	homePage = {
 		init: function(){
 			$.ajax({
@@ -333,6 +406,7 @@ jQuery(function($){
 					    }
 					  ]
 					});
+					hotsposts.init();
 					$(window).on('resize',function(){
 						$('#bathrooms-for-inspiration .part').matchHeight();						
 					});
@@ -466,27 +540,6 @@ jQuery(function($){
 				}
 			});
 		},
-		showHotspotData: function(obj){
-			var spot = $(obj).data("spot");
-			if($(obj).hasClass('active')){
-				$(obj).removeClass('active');
-				$('.hotspot-data.spot-'+spot).fadeOut();
-			}else{
-				$(obj).parent().find('.hotspot').removeClass('active');
-				$('.hotspot-data').fadeOut();
-				$(obj).addClass('active');
-				
-				var top = $(obj).position().top + $(obj).outerHeight()+10;
-				var left = $(obj).position().left - ($('.hotspot-data.spot-'+spot).outerWidth()/2) + ($(obj).outerHeight()/2);
-				$('.hotspot-data.spot-'+spot).css(
-				{
-					top: top,
-					left: left
-				})
-				$('.hotspot-data.spot-'+spot).fadeIn();
-				;
-			}
-		},
 		showerSetting: function(obj){
 			var type = $(obj).data('type');
 			$('#shower-settings .shower-image').removeClass('soft-rain intense-rain massage');
@@ -556,6 +609,7 @@ jQuery(function($){
 					    }
 					  ]
 					});
+					hotsposts.init();
 					$(window).on('resize',function(){
 						$('#bathrooms-for-inspiration .part').matchHeight();						
 					});
@@ -674,6 +728,7 @@ jQuery(function($){
 					    }
 					  ]
 					});
+					hotsposts.init();
 					$(window).on('resize',function(){
 						$('#bathrooms-for-inspiration .part').matchHeight();						
 					});
@@ -695,8 +750,14 @@ jQuery(function($){
   					arrows: false
 					})
 					$('#water-therapy .video .play').on('click', function(){
+						player.play();
 						$('#water-therapy .video-slider').slick('slickNext');
 					});
+					$('#water-therapy .close-video').on('click', function(){
+						player.stop();
+						$('#water-therapy .video-slider').slick('slickPrev');
+					});
+					player.init('methven-video','9SMTB_xeuM0');
 					loveCalm.tips();
 				}
 			});
@@ -849,7 +910,7 @@ jQuery(function($){
 			TweenMax.to($(this),0.5,{ scale: 1, ease: Elastic.easeOut });
 		});
 		moodboard.init();	
-		shareBoard.init();		
+		shareBoard.init();				
 	};
 	init();
 });

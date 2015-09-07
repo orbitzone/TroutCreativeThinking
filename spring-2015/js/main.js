ed23785fd9cbeb5b9dcee374f373695201bb183d
Diego Pelaez <diego@trout.com.au>
2015-09-07 11:24:16 +1000

    Updated bed judos styles and html.

--- spring-2015/js/main.js
+++ spring-2015/js/main.js
@@ -369,6 +369,7 @@ jQuery(function($){
 						slidesToShow: 1,
   					slidesToScroll: 1,
   					infinite: true,
+  					dots: true,
   					prevArrow: "<button type=\"button\" class=\"arrow-prev\"><i class=\"iconr-arrow-left\"></i></button>",
   					nextArrow: "<button type=\"button\" class=\"arrow-next\"><i class=\"iconr-arrow-right\"></i></button>",					 	
 					});									

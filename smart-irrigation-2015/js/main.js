0414b194ed07d37ef46f760ed8f24efbc587008c
Joey Kao <joey@trout.com.au>
2015-09-10 15:24:34 +1000

    smart irrigation - more dev done

--- smart-irrigation-2015/js/main.js
+++ smart-irrigation-2015/js/main.js
@@ -1,12 +1,23 @@
 $( document ).ready(function() {
     $('.slider').slick({
 	  infinite: true,
-	  prevArrow: '<span class="icon-chevron-left slick-prev"></span>',
-	  nextArrow: '<span class="icon-chevron-right slick-next"></span>',
+	  prevArrow: '<span class="fa fa-chevron-left slick-prev"></span>',
+	  nextArrow: '<span class="fa fa-chevron-right slick-next"></span>',
+	  dots: true,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	      }
+	    }
+	  ]
 
 	});
     $('.vertical-slider').slick({
-	  vertical: true
+	  vertical: true,
+	  dots: true,
+	  arrows: false
 	});
     $('.same-height').matchHeight();
 });

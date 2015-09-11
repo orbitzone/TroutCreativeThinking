5c7591e945819bf8b4e5175a25163ef69d089d2d
Diego Pelaez <diego@trout.com.au>
2015-09-11 17:07:08 +1000

    Updated product copy and links. Added produce json to manage share functionality.

--- spring-2015/js/main.js
+++ spring-2015/js/main.js
@@ -14,7 +14,7 @@ var fourStepsToCreate = {};
 var player = {};
 var mainUrl = "http://reece-spring.trout.com.au/";
 var isMobile = (function(a){return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
-
+var products = [];
 $.fn.imagesLoaded = function () {
 
     $imgs = this.find('img[src!=""]');
@@ -40,6 +40,16 @@ $.fn.imagesLoaded = function () {
 
 };
 
+function getProduct(code){
+	var product = null;
+	$.each(products, function(i, v) {
+    if (v.code == code) {
+    	product = v;
+      return true;
+    }
+	});
+	return product;
+}
 
 jQuery(function($){
 	player = {
@@ -120,14 +130,29 @@ jQuery(function($){
 		},
 		show: function(obj){
 			var $panel = $('#share-hex');
-			var data = {
-				url: mainUrl + $(obj).data('url'),
-				encodedUrl: encodeURIComponent(mainUrl + $(obj).data('url')),
-				img: mainUrl + $(obj).data('img'),
-				title: $(obj).data('title'),
-				via: $(obj).data('via'),
-				hashtags: $(obj).data('hashtags')
-			};
+
+			var product = $(obj).data('product-id');
+			if(typeof product !== "undefined"){
+				var productData = getProduct(product);
+				var data = {
+					url: productData.link,
+					encodedUrl: encodeURIComponent(productData.link),
+					img: mainUrl + productData.image,
+					title: productData.name,
+					via: productData.via,
+					hashtags: productData.hashtags
+				};
+				console.log(data);
+			}else{
+				var data = {
+					url: mainUrl + $(obj).data('url'),
+					encodedUrl: encodeURIComponent(mainUrl + $(obj).data('url')),
+					img: mainUrl + $(obj).data('img'),
+					title: $(obj).data('title'),
+					via: $(obj).data('via'),
+					hashtags: $(obj).data('hashtags')
+				};
+			}
 
 			$panel.find('.share-button.facebook').attr('href','http://www.facebook.com/sharer.php?u='+data.encodedUrl+'&title='+data.title);
 			$panel.find('.share-button.twitter').attr('href','https://twitter.com/share?url='+data.encodedUrl+'&text='+data.title+'&via='+data.via+'&hashtags='+data.hashtags);
@@ -153,7 +178,7 @@ jQuery(function($){
 			TweenMax.staggerTo($panel.find('.share-button .circle'),0.4,{ scale: 0, ease:Back.easeIn},0.1, function(){
 				TweenMax.to($panel,0.2,{ scale: 0});	
 			});					
-		},
+		}
 	};
 	moodboard = {
 		init: function(){
@@ -208,11 +233,11 @@ jQuery(function($){
 		add: function(obj, product){
 			moodboard.products += 1;
 			var $heart = $('<div class="heart-animation"><i class="icon icon-heart"></i></div>');
-			$('#main-content').append($heart);
-			var menuLeft = $('#destination-happiness .menu').offset().left - parseInt($('#main-content').css('margin-left')) +16;
-			var menuTop = $('#destination-happiness .menu').offset().top - $('#main-content').offset().top - 5;
-			var heartTop = $(obj).find('i').offset().top - $('#main-content').offset().top;
-			var heartLeft = $(obj).find('i').offset().left - parseInt($('#main-content').css('margin-left')) + 5; 
+			$('#main').append($heart);
+			var menuLeft = $('#destination-happiness .menu').offset().left + 15;
+			var menuTop = $('#destination-happiness .menu').offset().top- 5;
+			var heartTop = $(obj).find('i').offset().top ;
+			var heartLeft = $(obj).find('i').offset().left + 5; 
 
 			$heart.css({
 				position: 'absolute',
@@ -224,8 +249,8 @@ jQuery(function($){
 			TweenMax.to($heart, 1, {bezier:{type:"cubic", values:[{x:0, y:0}, {x:0, y: menuTop - heartTop}, {x: menuLeft - heartLeft, y: menuTop - heartTop}, {x: menuLeft - heartLeft, y: menuTop - heartTop }], autoRotate:["x","y","rotation", 0, true]}, scale:0.5, ease:Power1.easeInOut, onComplete: function(){ 
 			
 			//TweenMax.to($heart, 1, {left:$('#destination-happiness .menu').offset().left - $('#main-content').offset().left +16, top:$('#destination-happiness .menu').offset().top - $('#main-content').offset().top - 5, scale:0.5, ease:Power1.easeInOut, onComplete: function(){ 
-				$('#destination-happiness .dh-items').removeClass('empty').find('.number').html(moodboard.products);
-			$('#destination-links .dh-items').removeClass('empty').find('.number').html(moodboard.products);
+					$('#destination-happiness .dh-items').removeClass('empty').find('.number').html(moodboard.products);
+					$('#destination-links .dh-items').removeClass('empty').find('.number').html(moodboard.products);
 					$heart.fadeOut(200, function(){ $(this).remove(); });
 				}});
 			$(obj).addClass('added');			
@@ -1224,8 +1249,16 @@ jQuery(function($){
 	$(document).on('mouseout','.hex',function(){
 		TweenMax.to($(this),0.5,{scale: 1});
 	});
-	
 	function init(){
+		$.ajax({
+			url: 'json/products.json',
+			cache: false,
+			dataType: "json",
+			success: function(data){
+				products = data;
+				console.log(products);
+			}
+		})
 		$('html').addClass('spring-2015');
 		if($('body').hasClass('homepage')){
 			homePage.init();	

33cd913f1bb59fa5cfbcf377a8fb5f2123c66548
Dominic Milburn <dominic@trout.com.au>
2015-09-13 21:59:11 +1000

    - Update copy for smart thinking and text through out the smart irrigation site

--- smart-irrigation-2015/results.php
+++ smart-irrigation-2015/results.php
@@ -4,21 +4,25 @@
     array(
       "title"=>"Hydrozoning - Positioning Plants Wisely",
       "link"=>"smart-thinking-article1.html",
+        "text"=>"Many irrigation systems are not designed and set up according to the type of plant and its requirements. Unfortunately, a lot of irrigation systems tend to water turf and every type of plant in the landscape at the same time and rate. This method is inefficient and can cause waster wastage.",
       "thumbnail"=>"images/smart-thinking/article1.png"
     ),
     array(
       "title"=>"Why Irrigate?",
       "link"=>"smart-thinking-article2.html",
+        "text"=>"Irrigation systems are an efficient, easy and cost effective way to beautify homes and landscapes. Irrigation systems can contribute to increasing a property’s value and save time and money while helping to maintain the initial investment of landscapes.",
       "thumbnail"=>"images/smart-thinking/article2.png"
     ),
     array(
       "title"=>"Why to use smart irrigation systems.",
       "link"=>"smart-thinking-article3.html",
+        "text"=>"Being smart about irrigation is a good idea, both for the environment and your back pocket. Up to 50% of all water used in the home is for watering landscapes with the average property owner over-watering by 38%.",
       "thumbnail"=>"images/smart-thinking/article3.png"
     ),
     array(
       "title"=>"The benefits of Dripline",
       "link"=>"smart-thinking-article4.html",
+        "text"=>"Drip irrigation is an efficient and economical method of watering. Drip irrigation involves placing tubes with emitters in the garden bed or ground alongside the plants. Water is slowly dripped through the emitters directly into the soil at the root zone. ",
       "thumbnail"=>"images/smart-thinking/article1.png"
     ),
     array(

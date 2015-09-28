0a8ecd6a291a5c88602aa87f94f043aca0319651
Diego Pelaez <diego@trout.com.au>
2015-09-28 14:14:20 +1000

    Updated base tag to reece-responsive.trout.com.au

--- smart-irrigation-2015/templates/Includes/SmartIrrigation2015Meta.php
+++ smart-irrigation-2015/templates/Includes/SmartIrrigation2015Meta.php
@@ -1,4 +1,4 @@
-<base href="http://192.168.1.77/reece-responsive/smart-irrigation-2015/" />
+<base href="reece-responsive.trout.com.au/smart-irrigation-2015/" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta charset="utf-8">
 <meta name="viewport" content="initial-scale=1.0">

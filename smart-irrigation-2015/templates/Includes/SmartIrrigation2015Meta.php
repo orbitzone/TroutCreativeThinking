67a83f4feb370cdceef9a3cb187576074791583d
Joey Kao <joey@trout.com.au>
2015-09-30 14:58:17 +1000

    revert base

--- smart-irrigation-2015/templates/Includes/SmartIrrigation2015Meta.php
+++ smart-irrigation-2015/templates/Includes/SmartIrrigation2015Meta.php
@@ -1,4 +1,4 @@
-<base href="http://reece-responsive.local/smart-irrigation-2015/" /><!--[if IE]></base><![endif]-->
+<base href="http://reece-responsive.trout.com.au/smart-irrigation-2015/" /><!--[if IE]></base><![endif]-->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta charset="utf-8">
 <meta name="viewport" content="initial-scale=1.0">

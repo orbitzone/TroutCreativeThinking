a7c9d6504ef7af0f50ed92b94396b7255c575562
Joey Kao <joey@trout.com.au>
2015-10-13 15:52:18 +1100

    Added Bootstrap 3 Template folder

---
+++ bootstrap3-template/Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    server: {
+      port: grunt.option('port') || 8000
+    },
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: false,
+          optimization: 2
+        },
+        files: [
+          {
+              "css/reece-global.css": "less/reece-global.less",
+              "css/font-awesome.css": "less/font-awesome/font-awesome.less"
+          }
+        ]
+      }
+    },
+    copy: {
+      main: {
+          files: [
+
+              // setup bootstrap3 less files
+              {expand: true, cwd: 'bower_components/bootstrap/less', src: ['**'], dest: 'less/bootstrap'},
+              // setup fontawesome less files
+              {expand: true, cwd: 'bower_components/fontawesome/less', src: ['**'], dest: 'less/font-awesome'}
+          ]
+      }
+    },
+    watch: {
+      options: {
+        livereload: 35728
+      },
+      less: {
+        files: ['themes/*/less/*.less'],
+        tasks: ['less'],
+
+        options: {
+          spawn: false //important so that the task runs in the same context
+        }
+      }
+    }
+  });
+  //  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('default', ['copy', 'less', 'watch']);
+};
+
---
+++ bootstrap3-template/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js ie lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js ie lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>     <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>     <html class="no-js ie ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if IE 10]>    <html class="no-js ie ie10" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+  <base href="/bs3/" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="initial-scale=1.0">
+
+  <title>Reece BS3 Template</title>
+
+  <link rel="shortcut icon" href="images/icons/favicon.ico">
+  <link rel="apple-touch-icon" href="images/icons/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-touch-icon-114x114.png">
+
+  <script src='javascript/modernizr-2.6.2.js' type='text/javascript'></script>
+
+
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+    <link rel="stylesheet" type="text/css" href="css/reece-global.css" />
+</head>
+
+<body
+  class="">
+
+<div class="ocmain-wrapper">
+
+  <div class="browser-message">
+    <div class="content-container">
+      <div class="alert alert-warning" role="alert">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <p><strong>Attention! </strong>Your browser is out of date and may not fully support all features of this website. Learn how to <a href="http://browsehappy.com/">Upgrade Your Browser</a> to ensure the best experience.</p>
+      </div>
+    </div>
+  </div>
+
+  <?php include "templates/Includes/Navigation.php" ?>
+
+
+    <div class="content-container page">
+        <div class="inner-wrapper">
+
+            <div class="row-fluid">
+                <div class="span12">
+                    <h2>Reece BS3 Template</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis ipsum tortor, et aliquam erat egestas a. Aenean gravida, ante sed lobortis tristique, sem tortor pulvinar purus, quis fringilla nibh leo at dui. Phasellus tempus nec tellus a sodales. Sed sagittis sollicitudin urna, convallis porttitor mi. Proin sed vestibulum urna, at gravida dui. Proin ac tortor ultrices, accumsan diam tristique, maximus ipsum. Praesent pretium ligula a nibh scelerisque, sed placerat felis maximus. Cras quis euismod turpis. Sed vestibulum nisl nec tristique maximus. Aenean luctus lacus ut ex efficitur blandit. Vestibulum sed lobortis ipsum. Sed ornare facilisis nulla, eget placerat tellus maximus a.
+                    </p>
+                    <p>
+                        Vestibulum eget neque a mauris rutrum pellentesque. Vivamus ac viverra velit, vel varius enim. Vestibulum aliquet purus vel lorem finibus dictum id eget libero. Donec molestie egestas orci, ac accumsan eros vulputate vitae. Pellentesque semper nibh a metus lobortis consequat. Nulla vehicula ipsum id quam ullamcorper egestas. Morbi et aliquam nisi. Integer et laoreet elit.
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <?php include "templates/Includes/Footer.php" ?>
+</div>
+
+<?php include "templates/Includes/MobileNavigation.php" ?>
+
+<script type="text/javascript" src="javascript/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="javascript/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+<script type="text/javascript" src="javascript/reece-ocnav.js"></script>
+
+
+        </body>
+</html>

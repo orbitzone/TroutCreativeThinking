e0511fcab608f2909c449f4f29ead2fcfe06d1ec
Diego Pelaez <diego@trout.com.au>
2015-11-11 11:10:13 +1100

    Removed embeded css.

--- grant-2015/james.php
+++ grant-2015/james.php
@@ -112,7 +112,7 @@
                 <div class="row row-limit">
                   <div class="col-lg-12 spacing-bottom">
                     <div class="individual-video">
-                      <div class="embed-responsive embed-responsive-16by9">
+                      <div class="">
                         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Gh18sVLetRE?rel=0&amp;showinfo=0"></iframe>
                       </div>
                     </div>

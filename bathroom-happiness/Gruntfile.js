b51dcbc382eee304cc9584b66bce8e0e6beb6afe
Shane Houstein <shane.houstein@gmail.com>
2016-01-27 14:29:47 +1100

    Project setup

---
+++ bathroom-happiness/Gruntfile.js
@@ -0,0 +1,144 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      compass: {
+        files: ['sass/*.{scss,sass}'],
+        tasks: ['compass:dev']
+      },
+      js: {
+        files: ['js/*.js'],
+        tasks: ['jshint', 'uglify:dev']
+      },
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '*.html',
+          'css/style.css',
+          'js/*.js',
+          'images/{,**/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // Static Server
+    connect: {
+      server: {
+        options: {
+          port: 8000
+        }
+      }
+    },
+
+    // Compass
+    compass: {
+      options: {
+        //bundleExec: true,
+        httpPath: './',
+        cssDir: 'css',
+        sassDir: 'sass',
+        imagesDir: 'images',
+        javascriptsDir: 'js',
+        fontsDir: 'fonts',
+        assetCacheBuster: 'none',
+        // require: [
+        //   'sass-globbing'
+        // ]
+      },
+      dev: {
+        options: {
+          environment: 'development',
+          outputStyle: 'expanded',
+          relativeAssets: true,
+          raw: 'line_numbers = :true\n'
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production',
+          outputStyle: 'compact',
+          force: true
+        }
+      }
+    },
+ 
+    // Jshint
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'js/src/*.js'
+      ]
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          mangle: false,
+          compress: false,
+          preserveComments: 'all',
+          beautify: true
+        },
+        files: {
+          'js/main.min.js': [
+            'js/src/*.js'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          mangle: true,
+          compress: true
+        },
+        files: {
+          'js/main.min.js': [
+            'js/src/*.js'
+          ]
+        }
+      }
+    },
+
+    copy: {
+      js: {
+        files: [
+          { src: 'bower_components/angular/angular.min.js', dest: 'js/vendor/angular.min.js' },
+          { src: 'bower_components/jquery/dist/jquery.min.js', dest: 'js/vendor/jquery.min.js' },
+          { src: 'bower_components/respond/dest/respond.min.js', dest: 'js/vendor/respond.min.js' },
+          { src: 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js', dest: 'js/vendor/bootstrap.min.js' }
+        ]
+      }
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  
+  grunt.registerTask('build', [
+    'jshint',
+    'uglify:dist',
+    'compass:dist'
+  ]);
+
+  grunt.registerTask('default', [
+    'connect',
+    'jshint',
+    'uglify:dev',
+    'compass:dev',
+    'copy:js',
+    'watch'
+  ]);
+
+};
---
+++ bathroom-happiness/README.md
@@ -0,0 +1,30 @@
+A Trout Grunt/Compass Project
+=============================
+
+## Requirements
+* npm
+* Node.js
+* Rubygems
+* Sass
+* Compass
+
+Note: You do not need to have bower installed globally in order to run the project, as "bower install" is run with "npm install"
+
+
+## Setup
+1. Browse to the directory in terminal and install node modules:
+  > npm install
+
+2. Run grunt to init the Connect sever (a simple http server, used to test html files) and to start watching source files.
+  > grunt
+
+3. Navigate to local test server on the following URL:
+    
+  http://localhost:8000/
+
+Note: grunt watch must be running in order to access the server. Add "keepalive" to the connect config if you do not wish to keep watch running.
+
+
+## Production Build
+To build files in production mode (mangled JS files etc), use the following command:
+  > grunt build

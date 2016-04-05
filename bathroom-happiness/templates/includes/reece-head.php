fef95428a3963dd9c69adc46bbd64fe6e1cf94e3
Diego Pelaez <diego@trout.com.au>
2016-04-05 10:36:33 +1000

    Remove font

--- bathroom-happiness/templates/includes/reece-head.php
+++ bathroom-happiness/templates/includes/reece-head.php
@@ -7,7 +7,6 @@
         <script src="js/vendor/html5shiv/dist/html5shiv.js"></script>
     <![endif]-->
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6958312/7494952/css/fonts.css" />
     <link rel="stylesheet" type="text/css" href="css/reece-global.css" />
     <script src='js/vendor/jquery.min.js' type='text/javascript'></script>
     <script src="js/reece-ocnav.js" type="text/javascript"></script>

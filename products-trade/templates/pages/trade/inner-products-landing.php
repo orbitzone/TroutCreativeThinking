f47586afd8d56ea23e0bfe769118e8f829467d05
Diego Pelaez <diego@trout.com.au>
2016-05-17 10:58:12 +1000

    Add product landing page main files.

---
+++ products-trade/templates/pages/trade/inner-products-landing.php
@@ -0,0 +1,17 @@
+<!--START BREAD CRUMB -->
+<div class="breadcrumb-section bg-mid-grey section">
+	<div class="breadcrumb-inner">
+		<ul class="breadcrumbs">
+			<li><a href="#">Products</a></li>
+		</ul>
+	</div>
+</div>
+<!--END BREAD CRUMB -->
+<!--START PRODUCTS LANDING -->
+<div class="main-section" id="products-landing">
+	<?php include "templates/includes/trade/Widget.php" ?>
+	<div class="products-landing-wrap">
+		
+	</div>
+</div>
+<!--END PRODUCTS LANDING -->

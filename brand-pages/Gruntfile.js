8d112aed4fbc0f0364872d992a42816180c66f70
Diego Pelaez <diego@trout.com.au>
2016-06-01 14:32:55 +1000

    Create milli brand page. add styles and images.

--- brand-pages/Gruntfile.js
+++ brand-pages/Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function(grunt) {
               "css/brand-pages/armitage-shanks.css": "less/brand-pages/armitage-shanks/armitage-shanks.less",
               "css/brand-pages/rehau.css": "less/brand-pages/rehau/rehau.less",
               "css/brand-pages/roca.css": "less/brand-pages/roca/roca.less",
+              "css/brand-pages/milli.css": "less/brand-pages/milli/milli.less",
               "css/brand-pages/mizu.css": "less/brand-pages/mizu/mizu.less",
               "css/brand-pages/auspex.css": "less/brand-pages/auspex/auspex.less"
           }
---
+++ brand-pages/templates/brand-pages/milli.php
@@ -0,0 +1,393 @@
+<div class="brand-pages">
+  <div class="container-fluid">
+    <!-- Begin: Logo Widget Start -->
+    <div class="widget-logo">
+      <header role="banner">
+        <h1>
+          <a href="/">
+           <img src="images/brand-pages/milli/milli-logo.png" width="184" alt="Milli"><!-- Set the image width-->
+          </a>
+        </h1> 
+      </header>
+    </div>      
+    <!-- End:  Logo widget -->
+  </div>
+
+
+  <div class="container-fluid intro">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/milli-hero.gif" alt="Milli"/>            
+          </figure>
+          <div class="rte">
+            <h2>Meet Milli</h2>
+            <p>Milli is about confident style – enduring designs that enhance the way we live and naturally express who we are. Complete your look with Milli. Offering infinite possibilities.</p>
+          </div>                   
+        </div>
+        <!-- End: HTML Widget-->
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: Slider/Carousel Widget Start-->
+        <div class="widget-slider carousel">
+          <div class="slides" data-slides-to-show="5" data-slides-to-scroll="5">
+            <div class="slide">
+              <figure class="image">
+                  <img src="images/brand-pages/milli/axon-product.jpg" alt="Axon"/>
+              </figure>
+              <div class="text">
+               <h3>Axon</h3>
+               <p>Bold metallic and black finishes.</p>
+              </div>
+            </div>
+            <div class="slide">
+              <figure class="image">
+                <img src="images/brand-pages/milli/pure-product.jpg" alt="Pure"/>
+              </figure>
+               <div class="text">
+                 <h3>Pure</h3>
+                 <p>Perfectly embodies understated luxury.</p>
+              </div>
+            </div>
+            <div class="slide">
+              <figure class="image">
+                <img src="images/brand-pages/milli/glance-product.jpg" alt="Glance"/>
+              </figure>
+               <div class="text">
+                 <h3>Glance</h3>
+                 <p>Grace and presence with organic simplicity.</p>
+              </div>
+            </div>
+            <div class="slide">
+              <figure class="image">
+                <img src="images/brand-pages/milli/edge-product.jpg" alt="Edge"/>
+              </figure>
+               <div class="text">
+                 <h3>Edge</h3>
+                 <p>Bring emotion and energy to the space.</p>
+              </div>
+            </div>
+            <div class="slide">
+              <figure class="image">
+                <img src="images/brand-pages/milli/inox-product.jpg" alt="Inox"/>
+              </figure>
+               <div class="text">
+                 <h3>Inox</h3>
+                 <p>Live a beautiful life outdoors. The Milli Inox.</p>
+              </div>
+            </div>        
+          </div>
+        </div>    
+        <!-- End: Slider/Carousel Widget-->   
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container-fluid shop-the-range bg-green">
+    <div class="row no-padding">
+      <div class="col-sm-6 no-padding">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/hello-axon.jpg" alt="Hello Axon"/>            
+          </figure>                  
+        </div>
+        <!-- End: HTML Widget-->
+      </div>
+      <div class="col-sm-6 no-padding content">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Hello Axon</h3>
+            <p>Inspired by the perfection of a blade, Axon is a revolution in design. The juxtaposition of bold metallic and black finishes seamlessly embodied.</p>
+            <p><a href="" class="widget-link">Shop the axon range</a></p>
+          </div>                   
+        </div>
+        <!-- End: HTML Widget-->
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid shop-the-range bg-light-grey">
+    <div class="row no-padding">
+      <div class="col-sm-push-6 col-sm-6 no-padding">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/welcome-pure.jpg" alt="Welcome Pure"/>
+          </figure>                  
+        </div>
+        <!-- End: HTML Widget-->
+      </div>  
+      <div class="col-sm-pull-6 col-sm-6 no-padding content">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Welcome Pure</h3>
+            <p>Stripped of detail. Leaving only the essential. Pure is the very essence of great design. Effortless forms that perfectly embody the concept of understated luxury.</p>
+            <p><a href="" class="widget-link">Shop the pure range</a></p>
+          </div>                   
+        </div>
+        <!-- End: HTML Widget-->
+      </div>    
+    </div>
+  </div>
+  <div class="container-fluid shop-the-range bg-purple">
+    <div class="row no-padding">
+      <div class="col-sm-6 no-padding">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/hey-glance.jpg" alt="Hey Glance"/>            
+          </figure>                  
+        </div>
+        <!-- End: HTML Widget-->
+      </div>
+      <div class="col-sm-6 no-padding content">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Hey Glance</h3>
+            <p>The convergence of crisp lines and gentle arcs, Glance is a new expression of beauty. Grace and presence coming together with organic simplicity.</p>
+            <p><a href="" class="widget-link">Shop the glance range</a></p>
+          </div>                   
+        </div>
+        <!-- End: HTML Widget-->
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid shop-the-range bg-dark-grey">
+    <div class="row no-padding">
+      <div class="col-sm-push-6 col-sm-6 no-padding">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/enter-edge.jpg" alt="Enter Edge"/>            
+          </figure>                  
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+      <div class="col-sm-pull-6 col-sm-6 no-padding content">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Enter Edge</h3>
+            <p>Embracing modernist design principles, Edge is a geometric advenature. A bold interplay of angles bring emotion and energy to the space.</p>
+            <p><a href="" class="widget-link">Shop the edge range</a></p>
+          </div>
+        </div>
+        <!-- End: HTML Widget-->
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid homewares-accessories">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/homewares-accessories.jpg" alt="Homewares and Accessories"/>            
+          </figure>  
+          <div class="rte">
+            <h3>Homewares<br/>&amp; Accessories</h3>
+            <p>Capturing the innovative aesthetic of Milli tapware, our homewares collection allows you to extend your chosen look throughout every element of the bathroom.</p>
+            <p><a href="" class="widget-link">Shop the range</a></p>
+          </div>                
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+  <div class="container-fluid hey-inox">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/hey-inox.jpg" alt="Hey Inox"/>            
+          </figure>  
+          <div class="rte">
+            <h3>Hey Inox</h3>
+            <p>Live a beautiful life <br/>outdoors. The Milli Inox.</p>
+            <p><a href="" class="widget-link">Take me there</a></p>
+          </div>                
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+  
+  <div class="container-fluid reading-material-intro">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Reading<br/> Material</h3>
+            <p>Explore the Milli Bathroom Living and Milli Outdoor Living brochures.</p>
+          </div>                
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+  <div class="container-fluid reading-material">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: Slider/Carousel Widget Start-->
+        <div class="widget-slider carousel">
+          <div class="slides" data-slides-to-show="2" data-slides-to-scroll="2">
+            <div class="slide">
+              <figure class="image">
+                <a href="http://www.reece.com.au/assets/brands/820000/laufen-bathroom-products.pdf">
+                  <img src="images/brand-pages/milli/milli-bathroom-living.jpg" alt="Milli Bathroom Living PDF">  
+                </a>
+              </figure>
+              <div class="text">
+               <p>Download the Milli<br/>Bathroom Living PDF</p>
+               <a href="http://www.reece.com.au/assets/brands/820000/laufen-bathroom-products.pdf" class="widget-link">Download</a>
+              </div>
+            </div>
+            <div class="slide">
+              <figure class="image">
+                <a href="http://www.reece.com.au/assets/brands/820000/Laufen-SaphirKeramik.pdf">
+                  <img src="images/brand-pages/milli/milli-outdoor-living.jpg" alt="Milli Outdoor Living PDF">  
+                </a>
+              </figure>
+              <div class="text">
+                <p>Download the Milli<br/>Outdoor Living PDF</p>
+                <a href="http://www.reece.com.au/assets/brands/820000/Laufen-SaphirKeramik.pdf" class="widget-link">Download</a>
+              </div>
+            </div>    
+          </div>
+        </div>    
+        <!-- End: Slider/Carousel Widget-->   
+      </div>
+    </div>        
+  </div>
+  <div class="container-fluid video-intro">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>More<br/>About Milli</h3>
+            <p>Discover the infinite possibilities of the Milli range.</p>
+          </div>                
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+  <div class="container-fluid video">
+    <div class="row">
+      <div class="col-xs-12">
+         <!-- Begin: Video Widget-->
+        <div class="widget-video">
+          <div class="video-slides">
+            <div class="slide slide-front">
+              <figure class="image">
+                <img src="images/brand-pages/milli/more-about-milli.jpg" alt="More About Milli">
+              </figure>
+              <div class="slide-content">
+                <button type="button" class="play" data-video="231_3HKnAl4">
+                  <i class="rbp-icon"></i>                  
+                </button>  
+              </div>
+            </div>
+            <div class="slide slide-video">
+              <div class="video-wrapper">
+                <div class="video">
+                  <figure class="player">
+                  </figure>
+                </div>              
+              </div>
+              <button type="button" class="close">
+                <i class="rbp-close-icon"></i>               
+              </button>  
+            </div>
+          </div>    
+        </div>
+        <!-- End: Video Widget-->
+      </div>
+    </div>
+  </div>
+   <div class="container-fluid magic-metallics">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Magic<br/>Metallics</h3>
+            <p>Create a statement with Rose Gold, Matte Black, Chrome and Gun Metal.</p>
+          </div>                
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>       
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/be-bold.gif" alt="Be bold"/>            
+          </figure>             
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+  <div class="container-fluid daily-inspiration">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <div class="rte">
+            <h3>Daily<br/>Inspiration</h3>            
+          </div>                
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+  <div class="container-fluid find-inspiration">
+    <div class="row">
+      <div class="col-sm-6">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/milli/find-inspiration-on-instagram.jpg" alt="Instagram inspiration"/> 
+          </figure>
+          <div class="rte">
+             <h4>Find inspiration on Instagram</h4>
+            <p><a href="http://www.reece.com.au/assets/brands/820000/laufen-bathroom-products.pdf" class="widget-link">Instagram</a></p>
+          </div>               
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+      <div class="col-sm-6">
+        <!-- Begin: HTML Widget-->  
+        <div class="widget-html-image">
+           <figure class="image">
+           <img src="images/brand-pages/milli/meet-milli-on-pinterest.jpg" alt="Milli on Pinterest"/>
+          </figure>
+          <div class="rte">
+            <h4>Find inspiration on Instagram</h4>
+            <p><a href="http://www.reece.com.au/assets/brands/820000/laufen-bathroom-products.pdf" class="widget-link">Instagram</a></p>
+          </div>            
+        </div>
+        <!-- End: HTML Widget-->
+      </div>      
+    </div>
+  </div>
+</div>

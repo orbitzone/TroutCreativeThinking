12e5b0b69f423090a9b3db3bdc107229e1938ac1
Diego Pelaez <diego@trout.com.au>
2016-06-29 10:53:44 +1000

    Update styles. Add reece-base-template to repo

---
+++ reece-base-template/themes/templates/includes/Navigation.php
@@ -0,0 +1,96 @@
+<div class="navbar topNav">
+    <div class="navbar-container">
+        <a class="menuButton btn btn-default navbar-btn navbar-toggle hidden-desktop">
+            <div class="burger">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </div>
+            Menu
+        </a>
+
+        <a class="navbar-brand brand hidden-desktop mobile-logo" href="/"></a>
+
+        <div class="visible-desktop">
+            <div class="row">
+                <a href="/" class="navbar-brand brand"></a>
+
+                <div class="headerUtilityLinks">
+                    <ul class="navbar-nav nav">
+                        <li class="dropdown">
+
+
+                            <div class="logged-out content">
+                                <i class="fa fa-user">&nbsp;</i>
+
+                                <a href="/myaccount/home">Login</a>&nbsp;or&nbsp;<a href="/myaccount/home">Register</a>
+                            </div>
+
+
+                        </li>
+
+
+                    </ul>
+
+
+                    <div class="desktop-cart">
+                        <a href="/myaccount/cart">
+                            <i class="fa fa-shopping-cart">&nbsp;</i><span class="only-icon">Cart</span> (<span class="cart-count">0</span>)
+                        </a>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+            <div class="row navHeader">
+
+                <nav class="navWrap">
+
+
+                    <ul class="nav navbar-nav">
+
+                        <li class="link first active">
+                            <a href="/bathrooms">Bathrooms</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/plumbing">Plumbing</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/irrigation">Irrigation</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/civil">Civil</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/onsite">Onsite</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/hvacr">HVAC-R</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/aboutus">About Us</a>
+                        </li>
+
+                        <li class="link ">
+                            <a href="/storefinder">Store Finder</a>
+                        </li>
+
+                        <li class=" last">
+                            <a href="/myaccount/home">My Account</a>
+                        </li>
+
+                    </ul>
+
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>

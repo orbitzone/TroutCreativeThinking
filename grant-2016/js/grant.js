cd3024f090088bcf2f8a46dd359a36e881f01df0
Diego Pelaez <diego@trout.com.au>
2016-07-01 18:20:00 +1000

    Resize slider thumbs on different screens.

--- grant-2016/js/grant.js
+++ grant-2016/js/grant.js
@@ -200,7 +200,13 @@ var grant = {
       prevArrow: '<button type="button" class="slick-prev"><svg viewBox="0 0 26 46" id="shape-arrow-left"><title>arrow-left</title> <g id="arrow-left-arrow-left"> <path d="M24.4,0.6C24.1,0.2,23.5,0,23,0c-0.5,0-1,0.2-1.4,0.6l-21,21c-0.8,0.8-0.8,2.1,0,2.8l21,21c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L4.9,23L24.4,3.4C25.2,2.6,25.2,1.4,24.4,0.6z"/> </g> </svg></button>',
       nextArrow: '<button type="button" class="slick-next"><svg viewBox="0 0 26 46" id="shape-arrow-right"><title>arrow-right</title> <g id="arrow-right-arrow-right"> <path d="M1.6,45.4C2,45.8,2.5,46,3,46c0.5,0,1-0.2,1.4-0.6l21-21c0.8-0.8,0.8-2.1,0-2.8l-21-21c-0.8-0.8-2.1-0.8-2.8,0c-0.8,0.8-0.8,2.1,0,2.8L21.2,23L1.6,42.6C0.8,43.4,0.8,44.7,1.6,45.4z"/> </g> </svg></button>',
       focusOnSelect: true
-    });    
+    });
+    if($('.slider-thumbs').length>0){
+      $(window).on('resize', function(){
+        var height = $('.slider-thumbs .slide').width()/ 189 * 144;
+        $('.slider-thumbs .slide').height(height);
+      }).resize();
+    }
   },
   winnersSlider: function(){
     $('.winners-slider').slick({

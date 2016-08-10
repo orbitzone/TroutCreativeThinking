710e0e92fe1bf14911af7413bd9476f494ae169c
Diego Pelaez <diego@trout.com.au>
2016-08-10 12:39:40 +1000

    Update reece base template files.

--- reece-base-template/themes/templates/includes/Scripts.php
+++
@@ -1,8 +0,0 @@
-<!--START: FROM ORIGINAL TEMPLATE -->
-<script type="text/javascript" src="themes/reece-main/combined/reece_thirdparty.js"></script>
-<script type="text/javascript" src="themes/reece-main/javascript/bootstrap.min.js"></script>
-<script type="text/javascript" src="themes/reece-main/combined/reece_site.js"></script>
-<script type="text/javascript" src="themes/reece-main/javascript/thirdparty/jquery.base64.js"></script>
-<script type="text/javascript" src="themes/reece-main/javascript/jquery.history.1.8.js"></script>
-<script type="text/javascript" src="themes/reece-main/javascript/jquery.pagejax.js"></script>
-<!--END: FROM ORIGINAL TEMPLATE -->

a781881a697e63f57d702e6ad17aff177db3241d
Diego Pelaez <diego@trout.com.au>
2016-08-19 18:11:26 +1000

    Update bathroom happiness intro page to match the happiness project new design.

--- bathroom-happiness/index.php
+++ bathroom-happiness/index.php
@@ -25,35 +25,61 @@
             <div class="row">
               <!-- large/middle col (shower img) -->
               <div class="col-md-6 col-md-push-3">
-                <div class="panel panel-hero">
-                  <a href="inspiration.php">
-                    <div class="image">
-                      <img src="images/homepage/get-inspired-bathroom.png" alt="get inspired bathroom">
-                    </div>
-                    <h2 class="intro-text">
-                      it's time to<br>
-                      create a bathroom<br/>
-                      you love
-                    </h2>
-                    <div class="experience">
-                      Experience a new <br/>world of inspiration
-                    </div>
-                    <div class="arrow-link">
-                        <div class="arrow-link-label">Get Inspired</div>
-                        <div class="arrow-right">
-                          <span class="arrow-link-icon"><i class="arrow-link-icon icon-arrow-right"></i></span>
+                <div class="panel panel-hero panel-hero-happiness">
+                  <div class="image">
+                    <img src="images/homepage/the-happiness-project.jpg" alt="The Happiness project">
+                  </div>
+                  <div class="content-wrap">
+                    <a href="inspiration.php">
+                      <span class="border-top"></span>
+                      <span class="borders"></span>
+                      <div class="the-happiness-project-logo">
+                        <svg width="269.4px" height="111.4px" viewBox="0 0 269.4 111.4" enable-background="new 0 0 269.4 111.4" xml:space="preserve">
+                          <path d="M5.9,28.4v10h10.3v-10h5.6v25.3h-5.6V43.5H5.9v10.2H0.3V28.4H5.9z M37.8,53.7h-5.7l10.8-25.5h5.1l10.8,25.5h-5.8L50.8,48
+                          H40.1L37.8,53.7z M42.1,43.1h6.7l-3.4-8.2L42.1,43.1z M75.6,53.7H70V28.4h10.3c6,0,9.7,3.6,9.7,8.7v0.1c0,5.9-4.6,8.9-10.2,8.9h-4.2
+                          V53.7z M75.6,41.1H80c2.8,0,4.4-1.7,4.4-3.8v-0.1c0-2.5-1.7-3.8-4.5-3.8h-4.3V41.1z M107.7,53.7h-5.6V28.4h10.3c6,0,9.7,3.6,9.7,8.7
+                          v0.1c0,5.9-4.6,8.9-10.2,8.9h-4.2V53.7z M107.7,41.1h4.4c2.8,0,4.4-1.7,4.4-3.8v-0.1c0-2.5-1.7-3.8-4.5-3.8h-4.3V41.1z M134.4,53.7
+                          h5.6V28.4h-5.6V53.7z M159,37.6l12.2,16.1h4.7V28.4h-5.5v15.6l-11.9-15.6h-5.1v25.3h5.5V37.6z M208.4,33.3v-4.9h-19.1v25.3h19.3v-5
+                          h-13.7v-5.3h11.9v-4.9h-11.9v-5.1H208.4z M229.8,49.1c-2.9,0-5.3-1.2-7.5-3.1L219,50c3,2.7,6.9,4,10.7,4c5.5,0,9.3-2.8,9.3-7.8v-0.1
+                          c0-4.4-2.9-6.3-8-7.6c-4.4-1.1-5.5-1.7-5.5-3.3v-0.1c0-1.2,1.1-2.2,3.3-2.2c2.1,0,4.3,0.9,6.6,2.5l2.9-4.2c-2.6-2.1-5.7-3.2-9.4-3.2
+                          c-5.2,0-8.9,3-8.9,7.6v0.1c0,5,3.3,6.4,8.4,7.7c4.2,1.1,5.1,1.8,5.1,3.2v0.1C233.5,48.2,232.1,49.1,229.8,49.1z M259.8,49.1
+                          c-2.9,0-5.3-1.2-7.5-3.1L249,50c3,2.7,6.9,4,10.7,4c5.5,0,9.3-2.8,9.3-7.8v-0.1c0-4.4-2.9-6.3-8-7.6c-4.4-1.1-5.5-1.7-5.5-3.3v-0.1
+                          c0-1.2,1.1-2.2,3.3-2.2c2.1,0,4.3,0.9,6.6,2.5l2.9-4.2c-2.6-2.1-5.7-3.2-9.4-3.2c-5.2,0-8.9,3-8.9,7.6v0.1c0,5,3.3,6.4,8.4,7.7
+                          c4.2,1.1,5.1,1.8,5.1,3.2v0.1C263.5,48.2,262.1,49.1,259.8,49.1z M21.4,93.4h-5.6V68.1h10.3c6,0,9.7,3.6,9.7,8.7v0.1
+                          c0,5.9-4.6,8.9-10.2,8.9h-4.2V93.4z M21.4,80.9h4.4c2.8,0,4.4-1.7,4.4-3.8V77c0-2.5-1.7-3.8-4.5-3.8h-4.3V80.9z M55.7,93.4h-5.6
+                          V68.1h11.6c6,0,9.5,3.1,9.5,8.3v0.1c0,4.1-2.2,6.6-5.4,7.8l6.2,9h-6.5L60,85.3h-4.4V93.4z M55.7,80.4h5.6c2.7,0,4.3-1.4,4.3-3.6
+                          v-0.1c0-2.4-1.7-3.6-4.4-3.6h-5.5V80.4z M100.1,67.7c7.8,0,13.4,5.8,13.4,13v0.1c0,7.2-5.7,13.1-13.5,13.1c-7.8,0-13.4-5.8-13.4-13
+                          v-0.1C86.7,73.6,92.3,67.7,100.1,67.7z M92.5,80.7c0,4.3,3.2,7.9,7.7,7.9c4.5,0,7.6-3.5,7.6-7.9v-0.1c0-4.3-3.2-7.9-7.7-7.9
+                          C95.6,72.8,92.5,76.3,92.5,80.7L92.5,80.7z M134.7,93.8c5.3,0,9-3,9-9.1V68.1h-5.7v16.3c0,2.9-1.3,4.2-3.4,4.2
+                          c-1.8,0-3.2-0.9-4.6-2.5l-3.5,3.9C128.2,92,130.8,93.8,134.7,93.8z M180,73.1v-4.9h-19.1v25.3h19.3v-5h-13.7v-5.3h11.9v-4.9h-11.9
+                          v-5.1H180z M208.7,72.8c2.5,0,4.5,1.1,6.5,2.9l3.5-4.1c-2.3-2.3-5.2-3.9-10-3.9c-7.8,0-13.2,5.9-13.2,13.1v0.1c0,7.3,5.5,13,13,13
+                          c4.9,0,7.8-1.7,10.4-4.5l-3.5-3.6c-2,1.8-3.8,3-6.6,3c-4.3,0-7.3-3.6-7.3-7.9v-0.1C201.3,76.3,204.4,72.8,208.7,72.8z M233.1,73.2
+                          h7.7v20.2h5.6V73.2h7.7v-5.1h-21V73.2z M96.9,4.7H0v2.2h96.9V4.7z M0,111.4h269.4v-2.2H0V111.4z M171.1,4.7v2.2h98.2V4.7H171.1z
+                           M114.2,12.2h2.7V2.5h3.7V0h-10.1v2.5h3.7V12.2z M129.5,7.3h4.9v4.9h2.7V0h-2.7v4.8h-4.9V0h-2.7v12.2h2.7V7.3z M153.5,9.8h-6.6V7.2
+                          h5.7V4.8h-5.7V2.4h6.5V0h-9.2v12.2h9.3V9.8z"/>
+                        </svg>
+                      </div>
+                      <div class="text-content">
+                        <h2 class="intro-text">
+                          it's time to create a bathroom you love
+                        </h2>
+                        <div class="arrow-link">
+                            <div class="arrow-link-label">Get Started</div>                        
                         </div>
-                    </div>
-                  </a>
+                      </div>
+                    </a>
+                  </div>                  
                 </div>
               </div>
               <!-- left col (find inspiration img) -->
               <div class="col-md-3 col-sm-6 col-md-pull-6">
-                <a href="inspiration.php" class="panel panel-link panel-find-inspiration">
+                <a href="inspiration.php" class="panel panel-link panel-luxe-on-a-budget">
                   <div class="background-div"></div>
                   <div class="text-overlay">
-                    <div class="text-overlay-line-1">Find</div> 
-                    <div class="text-overlay-line-2">Inspiration</div>
+                    <div class="text-overlay-image"><img src="images/homepage/shaynna-blaze.png" alt="Shaynna Blaze"></div>
+                    <div class="text-overlay-line-0 title-a">luxe on a budget</div> 
+                    <div class="text-overlay-line-1">with</div> 
+                    <div class="text-overlay-line-2">Shaynna<br/>Blaze</div>
                     <div class="arrow-link">
                       <span class="arrow-link-icon"><i class="arrow-link-icon icon-arrow-right"></i></span>
                     </div>
@@ -120,11 +146,11 @@
                   </div>
                   </form>
                 </div>
-                <a href="http://www.reece.com.au/bathrooms/products" class="panel-link panel panel-shop-products">
+                <a href="http://www.reece.com.au/bathrooms/products" class="panel-link panel panel-crushing-on-tapware">
                   <div class="background-div"></div>
                   <div class="text-overlay">
-                    <div class="text-overlay-line-1">Shop</div> 
-                    <div class="text-overlay-line-2">Products</div>
+                    <div class="text-overlay-line-1 title-a">crushing on</div> 
+                    <div class="text-overlay-line-2">Tapware</div>
                     <div class="arrow-link">
                       <span class="arrow-link-icon"><i class="arrow-link-icon icon-arrow-right"></i></span>
                     </div>

c5300670fa1f74410e91a3e763ba84d74d1a709e
Victoria Houen <victoria.houen@trout.com.au>
2016-08-21 11:41:14 +1000

    brand pages - conex

--- brand-pages/index.php
+++ brand-pages/index.php
@@ -1,47 +1,142 @@
+
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js ie lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js ie lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>     <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en"> <![endif]-->
-<!--[if IE 9]>     <html class="no-js ie ie9 lt-ie10" lang="en"> <![endif]-->
-<!--[if IE 10]>    <html class="no-js ie ie10" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html lang="en">
 <head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta charset="utf-8">
-  <meta name="viewport" content="initial-scale=1.0">
-  <title>Friatec</title>
-  <link rel="shortcut icon" href="images/icons/favicon.ico">
-  <link rel="apple-touch-icon" href="images/icons/apple-touch-icon.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-touch-icon-114x114.png">
-  <script src='javascript/modernizr-2.6.2.js' type='text/javascript'></script>
-  <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6958312/662428/css/fonts.css" />
-  <link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
-  <link rel="stylesheet" type="text/css" href="css/reece-global.css" />
-  <link rel="stylesheet" type="text/css" href="css/brand-pages/widgets.css" />
-  <link rel="stylesheet" type="text/css" href="css/brand-pages/friatek.css" />
+	<meta charset="utf-8" />
+	<title>Reece Responsive Project Index</title>
+	<style type="text/css">
+	 * {
+	font-family: 'Helvetica Neue', Arial;
+	}
+
+	ul li
+	{
+		list-style: none;
+		line-height: 1.2em;
+	}
+	ul li:before
+	{
+		content: '\2713';
+		margin: 0 1em 0 0;
+	}
+
+	.wip li:before, li.wip:before
+	{
+		content: '\1F354';
+	}
+
+	.wip a
+	{
+		color: red !important;
+	}
+
+
+	</style>
 </head>
-<body class="brand-pages">
-  <div class="ocmain-wrapper">
-    <div class="browser-message">
-      <div class="content-container">
-        <div class="alert alert-warning" role="alert">
-          <button type="button" class="close" data-dismiss="alert">&times;</button>
-          <p><strong>Attention! </strong>Your browser is out of date and may not fully support all features of this website. Learn how to <a href="http://browsehappy.com/">Upgrade Your Browser</a> to ensure the best experience.</p>
-        </div>
-      </div>
-    </div>
-    <?php include "templates/Includes/Navigation.php" ?>
-    <?php include "templates/brand-pages/friatek.php" ?>  
-    <?php include "templates/Includes/Footer.php" ?>
-  </div>
-  <?php include "templates/Includes/MobileNavigation.php" ?>
-  <script type="text/javascript" src="javascript/jquery-1.11.2.min.js"></script>
-  <script type="text/javascript" src="javascript/jquery-migrate-1.2.1.min.js"></script>
-  <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
-  <script type="text/javascript" src="javascript/slick.min.js"></script>
-  <script type="text/javascript" src="javascript/reece-ocnav.js"></script>
-  <script type="text/javascript" src="javascript/jquery.matchHeight-min.js"></script>
-  <script type="text/javascript" src="javascript/brand-pages/widgets.js"></script>
-  </body>
+<body>
+	<h1>Reece Responsive Project Index</h1>
+	<h2>Global</h2>
+	<ul>
+		<li><a href="home/">Reece Homepage</a></li>
+	</ul>
+	<h2>Navigation 2014</h2>
+	<ul class="wip">
+		<li><a href="navigation/new-nav.php">Home</a></li>
+		<li><a href="navigation/alt-search.php">Alt. Search</a></li>
+		<li><a href="navigation/happiness.php">Bathrooms</a></li>
+		<li><a href="navigation/happiness-log-in.php">Retail Log In</a></li>
+		<li><a href="navigation/">Trade</a></li>
+		<li class="wip"><a href="plumbing/index-log-in.php">My Account login</a></li>
+		<li><a href="navigation/new-subnav.php">Corporate</a></li>
+	</ul>
+	<h2>Bathrooms/Retail</h2>
+	<ul>
+		<li><a href="bathroom/3D-planner">3D Planner</a></li>
+		<li><a href="bathroom/bathroom-moments">Bathroom Moments</a></li>
+		<li><a href="bathroom/brands">Brands</a></li>
+		<li><a href="bathroom/brands/laufen">Brands / Complex</a></li>
+		<li><a href="bathroom/brands/ideal-standard">Brands / Simple</a></li>
+		<li><a href="bathroom/happiness-zone">Bathrooms Landing Page "Happiness Zone"</a></li>
+		<li><a href="bathroom/products">Products</a></li>
+		<li><a href="bathroom/products/basins">Products / Category</a></li>
+		<li><a href="bathroom/products/basins/wall-mounted">Products / Detail</a></li>
+		<li class="wip"><a href="bathroom/products/addlist">Products / Add To List</a></li>
+		<li class="wip"><a href="bathroom/quote-order">Quote To Order</a></li>
+		<li><a href="bathroom/srl-event">Event</a></li>
+		<li class="wip"><a href="ilmb">I Love My Bathroom 2014</a></li>
+		<li class="wip"><a href="the-block-fans-v-faves">The Block / Fans v Faves</a></li>
+		<li class="wip"><a href="theblock2014-tvc/index.php">The Block 2014 - TVC</a></li>
+		<li class="wip"><a href="bathroom/inspiration">Inspiration Landing</a></li>
+		<li class="wip"><a href="bathroom/inspiration/gallery">Inspiration > Gallery</a></li>
+		<li class="wip"><a href="bathroom/inspiration/designerlooks">Inspiration > Designer Looks</a></li>
+		<li class="wip"><a href="bathroom/inspiration/designerlooks/cameron-bruhn.php">Inspiration > Designer Looks > Cameron Bruhn</a></li>
+		<li class="wip"><a href="bathroom/inspiration/designerlooks/nicky-lobo.php">Inspiration > Designer Looks > Nicky Lobo</a></li>
+		<li class="wip"><a href="bathroom/inspiration/trends-day-spa.php">Inspiration > Day Spa</a></li>
+
+		<li class="wip"><a href="bathroom/inspiration/trends-hotel-inspired.php">Inspiration > Hotel Inspired</a></li>
+		<li class="wip"><a href="bathroom/inspiration/trends-3.php">Inspiration > Trends 3</a></li>
+		<li class="wip"><a href="bathroom/inspiration/trends-4.php">Inspiration > Trends 4</a></li>
+		<li class="wip"><a href="bathroom/spring2014/">The Spring Bathroom 2014 Splash</a></li>
+	</ul>
+	<h2>Brand Pages</h2>
+	<ul>
+		<li class="wip"><a href="brand-pages/index-american-standard.php" target="_blank">American Standard</a></li>
+		<li class="wip"><a href="brand-pages/index-armitage-shanks.php" target="_blank">Armitage Shanks</a></li>
+		<li class="wip"><a href="brand-pages/index-auspex.php" target="_blank">Auspex</a></li>
+		<li class="wip"><a href="brand-pages/index-conex.php" target="_blank">Conex</a></li>
+		<li class="wip"><a href="brand-pages/index-friatec.php" target="_blank">Friatec</a></li>
+		<li class="wip"><a href="brand-pages/index-kado.php" target="_blank">Kado</a></li>
+		<li class="wip"><a href="brand-pages/index-laufen.php" target="_blank">Laufen</a></li>
+		<li class="wip"><a href="brand-pages/index-milli.php" target="_blank">Milli</a></li>
+		<li class="wip"><a href="brand-pages/index-mizu.php" target="_blank">Mizu</a></li>
+		<li class="wip"><a href="brand-pages/index-rehau-childrens-hospital.php" target="_blank">Rehau - Children's Hospital</a></li>
+		<li class="wip"><a href="brand-pages/index-rehau-fiona-stanley.php" target="_blank">Rehau - Fiona Stanley</a></li>
+		<li class="wip"><a href="brand-pages/index-rehau-joynton-ave.php" target="_blank">Rehau - Joynton Ave</a></li>
+		<li class="wip"><a href="brand-pages/index-rehau-orrong-road.php" target="_blank">Rehau - Orrong Road</a></li>
+		<li class="wip"><a href="brand-pages/index-rehau.php" target="_blank">Rehau</a></li>
+		<li class="wip"><a href="brand-pages/index-roca.php" target="_blank">Roca</a></li>
+		<li class="wip"><a href="brand-pages/index-wolfen.php" target="_blank">Wolfen</a></li>
+	</ul>	
+	<h2>Plumbing/Trade</h2>
+	<ul>
+		<li class="wip"><a href="plumbing">Plumbing Landing Page</a></li>
+		<li class="wip"><a href="plumbing/products">Products</a></li>
+		<li><a href="plumbing/products/hot-water-units">Products / Category</a></li>
+		<li><a href="plumbing/diy-disasters">DIY Disasters</a></li>
+		<li class="wip"><a href="outlet">Outlet 2014</a></li>
+		<li class="wip"><a href="outlet/article">Outlet / Article</a></li>
+		<li class="wip"><a href="outlet/previous-articles">Outlet / Previous Issues</a></li>
+		<li class="wip"><a href="outlet/the-crack">Outlet / Section Index</a></li>
+		<li class="wip"><a href="outlet/selected-trade-plumbing">Outlet / Plumbing Selected</a></li>
+		<li class="wip"><a href="outlet/selected-trade-civil">Outlet / Civil Selected</a></li>
+		<li class="wip"><a href="outlet/selected-trade-hvacr">Outlet / hvac-r Selected</a></li>
+		<li class="wip"><a href="outlet/selected-trade-irrigation">Outlet / Irrigation Selected</a></li>
+	</ul>
+	<h2>Speciality</h2>
+	<ul>
+		<li class="wip"><a href="civil">Civil</a></li>
+		<li class="wip"><a href="hvacr">hvac-r</a></li>
+		<li><a href="hvacr/gas-track">hvac-r / Gas Track</a></li>
+		<li><a href="hvacr/promotions">hvac-r / Promotions</a></li>
+		<li class="wip"><a href="onsite">Onsite</a></li>
+		<li class="wip"><a href="irrigation">Irrigation</a></li>
+		<li class="wip"><a href="hitachi-summer">Hitachi Summer Rewards</a></li>
+		<li class="wip"><a href="smart-irrigation-2015">Smart Irrigation 2015</a></li>
+		<li class="wip"><a href="grant-2015">Grant 2015</a></li>
+	</ul>
+
+	<h2>About Us</h2>
+	<ul>
+		<li class="wip"><a href="aboutus">About Us Landing page</a>
+		<ul>
+		<li class="wip"><a href="/aboutus/partner.php">Partner with Reece</a></li>
+		<li class="wip"><a href="/aboutus/history.php">History</a></li>
+		<li class="wip"><a href="/aboutus/finance.php">Finance</a></li>
+		<li class="wip"><a href="/aboutus/contact.php">Contact Us</a></li>
+		</ul>
+		</li>
+
+	</ul>
+	<p class="wip"><a id="top">*not deployed yet</a></p>
+</body>
 </html>
--- brand-pages/templates/brand-pages/conex.php
+++ brand-pages/templates/brand-pages/conex.php
@@ -5,7 +5,7 @@
       <header role="banner">
         <h1>
           <a href="/">
-           <img src="images/brand-pages/conex/conex-banner.jpg" alt="conex"><!-- Set the image width-->
+            <img src="images/brand-pages/conex/conex-banner.jpg" alt="conex"><!-- Set the image width-->
           </a>
         </h1> 
       </header>
@@ -18,149 +18,284 @@
       <div class="col-sm-6">
         <!-- Begin: HTML Widget-->  
         <div class="widget-html-image">
-          <!-- If no image dont show
-          <figure class="image">
-            <a href="#">
-            <img />
-            </a>
-          </figure>
-          -->
-          <div class="rte">
-            <h3>Discover what makes us the industry leader.</h3>            
-          </div>
-        </div>
-        <!-- End: HTML Widget-->
-      </div>
-      <div class="col-sm-6">
-        <!-- Begin: HTML Widget-->  
-        <div class="widget-html-image">
-          <!-- If no image dont show
-          <figure class="image">
-            <a href="#">
-            <img />
-            </a>
-          </figure>
-          -->
-          <div class="rte">
-            <p>Passionate about excellence, Conex Bänninger is the name professionals trust for domestic, commercial, industrial, air conditioning and refrigeration applications worldwide. Conex Bänninger is an ISO 9000 assured company, which assures you the very best in quality.</p>
-          </div>
-        </div>
-        <!-- End: HTML Widget-->
-      </div>
-    </div>
-  </div>
+<!-- If no image dont show
+<figure class="image">
+<a href="#">
+<img />
+</a>
+</figure>
+-->
+<div class="rte">
+  <h3>Discover what makes us the industry leader.</h3>            
+</div>
+</div>
+<!-- End: HTML Widget-->
+</div>
+<div class="col-sm-6">
+  <!-- Begin: HTML Widget-->  
+  <div class="widget-html-image">
+<!-- If no image dont show
+<figure class="image">
+<a href="#">
+<img />
+</a>
+</figure>
+-->
+<div class="rte">
+  <p>Passionate about excellence, Conex Bänninger is the name professionals trust for domestic, commercial, industrial, air conditioning and refrigeration applications worldwide. Conex Bänninger is an ISO 9000 assured company, which assures you the very best in quality.</p>
+</div>
+</div>
+<!-- End: HTML Widget-->
+</div>
+</div>
+</div>
 
-  <div class="container-fluid conex-video">
-    <div class="row">
-      <div class="col-xs-12">
-        <!-- Begin: Video Widget-->  
-        <div class="widget-video">
-          <div class="video-slides">
-            <div class="slide slide-front">
-              <div class="image">
-                <img src="images/brand-pages/conex/video-placeholder.jpg" alt=">B< Press Press Fit System">
-              </div>
-              <div class="slide-content">
-                <button type="button" class="play" data-video="_0TBVfMLoc8">
-                  <i class="rbp-icon"></i>
-                  <!--<span class="label"></span>  -->
-                </button>  
-              </div>
+<div class="container-fluid conex-video">
+  <div class="row">
+    <div class="col-xs-12">
+      <!-- Begin: Video Widget-->  
+      <div class="widget-video">
+        <div class="video-slides">
+          <div class="slide slide-front">
+            <div class="image">
+              <img src="images/brand-pages/conex/video-placeholder.jpg" alt=">B< Press Press Fit System">
             </div>
-            <div class="slide slide-video">
-              <div class="video-wrapper">
-                <div class="video">
-                  <figure class="player"></figure>
-                </div>
-              </div>
-              <button type="button" class="close">
-                <i class="rbp-close-icon"></i>               
+            <div class="slide-content">
+              <button type="button" class="play" data-video="_0TBVfMLoc8">
+                <i class="rbp-icon"></i>
+                <!--<span class="label"></span>  -->
               </button>  
             </div>
-          </div>    
-        </div>
-        <!-- End: Video Widget-->
+          </div>
+          <div class="slide slide-video">
+            <div class="video-wrapper">
+              <div class="video">
+                <figure class="player"></figure>
+              </div>
+            </div>
+            <button type="button" class="close">
+              <i class="rbp-close-icon"></i>               
+            </button>  
+          </div>
+        </div>    
       </div>
+      <!-- End: Video Widget-->
     </div>
   </div>
-    
-  <div class="container-fluid products">
-    <div class="row">
-      <div class="col-sm-12">
-        <!-- Begin: HTML Widget-->  
-        <div class="widget-html-image">
-          <!-- If no image dont show
-          <figure class="image">
-            <a href="#">
-            <img />
-            </a>
-          </figure>
-          -->
-          <div class="rte">
-            <h3><!--<a href="#">-->Products profiles<!--</a>--></h3>
+</div>
+
+<div class="container-fluid products">
+  <div class="row">
+    <div class="col-sm-12">
+      <!-- Begin: HTML Widget-->  
+      <div class="widget-html-image">
+<!-- If no image dont show
+<figure class="image">
+<a href="#">
+<img />
+</a>
+</figure>
+-->
+<div class="rte">
+  <h3><!--<a href="#">-->PRODUCT PROFILES<!--</a>--></h3>
+</div>
+</div>
+<!-- End: HTML Widget-->
+</div>
+</div>
+<div class="row">
+  <div class="col-sm-6">
+    <!-- Begin: HTML Widget-->  
+    <div class="widget-html-image">
+      <figure class="image">
+        <img src="images/brand-pages/conex/parallax-bpress-500x450.png" width="300px" alt="B Press 15-50mm"/>
+      </figure>          
+    </div>
+    <!-- End: HTML Widget-->
+  </div>
+  <div class="col-sm-6">
+    <!-- Begin: HTML Widget-->
+    <div class="even"> 
+      <h4>B PRESS 15-50mm</h4>
+      <p>Conex Bänninger is an ISO 9000 quality &gt;B&lt; Press is a versatile press fitting system for use with hard, half hard or soft 
+        copper tube pro assured company, which assures you the very best in quality. 
+        Conex Bänningerviding a secure, life long, leak-proof joint.</p>
+        <div class="ctaBox">
+          <div class="viewBtn">view range</div>
+        </div>
+      </div>
+      <!-- End: HTML Widget-->
+    </div>
+  </div>
+</div>
+<!-- ./end row -->
+
+<div class="container-fluid products">
+  <div class="row">
+    <div class="col-sm-6">
+      <!-- Begin: HTML Widget-->  
+      <div class="odd">
+        <h4>B Press XL 65-100mm</h4>
+        <p>&gt;B&lt; Press XL has only two internal parts, the grip ring and the O-ring. 
+          Both parts have an internal diameter larger than the tube, which allows for easy tube insertion and pre press leak indication.</p>
+          <div class="ctaBox odd">
+            <div class="viewBtn odd">view range</div>
           </div>
+        </div>
+      </div>
+      <!-- End: HTML Widget-->
+      <div class="col-sm-6">
+        <!-- Begin: HTML Widget-->
+        <div class="widget-html-image">
+          <figure class="image">
+            <img src="images/brand-pages/conex/parallax-bpress-XL-500x450.png" width="300px" alt="B Press 15-50mm"/>
+          </figure>
         </div>
         <!-- End: HTML Widget-->
       </div>
     </div>
+  </div>
+  <!-- ./end row -->
+
+  <div class="container-fluid products">
     <div class="row">
       <div class="col-sm-6">
         <!-- Begin: HTML Widget-->  
         <div class="widget-html-image">
           <figure class="image">
-            <a href="http://www.reece.com.au/new/pdf/products/Conex-Banninger-technical-guide.pdf">
-            <img src="images/brand-pages/conex/b-press.jpg" alt="B Press 15-50mm"/>
-            <figcaption>B Press 15-50mm</figcaption>
-            </a>
+            <img src="images/brand-pages/conex/maxipro.jpg" width="300px" alt="B Press 15-50mm"/>
           </figure>          
         </div>
         <!-- End: HTML Widget-->
       </div>
       <div class="col-sm-6">
-        <!-- Begin: HTML Widget-->  
+        <!-- Begin: HTML Widget-->
+        <div class="even"> 
+          <h4>Maxipro</h4>
+          <p>MaxiPro press fittings use a high-quality HNBR O-ring that forms a secure leak-free joint when pressed. 
+            The innovative lead-in edge design grips the pipe to aid installation while helping to protect the O-ring 
+            from damage or displacement.</p>
+            <div class="ctaBox">
+              <div class="viewBtn">view range</div>
+            </div>
+          </div>
+          <!-- End: HTML Widget-->
+        </div>
+      </div>
+    </div>
+    <!-- ./end row -->
+
+    <!-- left side video 1 of 2 -->
+    <div class="container-fluid conex-video">
+      <div class="row">
+        <div class="col-sm-6">
+          <!-- Begin: Video Widget-->  
+          <div class="widget-video">
+            <div class="video-slides">
+              <div class="slide slide-front">
+                <div class="image">
+                  <img src="images/brand-pages/conex/video-placeholder-1.jpg" alt=">Installation Process">
+                </div>
+                <div class="slide-content">
+                  <button type="button" class="play" data-video="_0TBVfMLoc8">
+                    <i class="rbp-icon"></i>
+                    <!--<span class="label"></span>  -->
+                  </button>  
+                </div>
+              </div>
+              <div class="slide slide-video">
+                <div class="video-wrapper">
+                  <div class="video">
+                    <figure class="player"></figure>
+                  </div>
+                </div>
+                <button type="button" class="close">
+                  <i class="rbp-close-icon"></i>               
+                </button>  
+              </div>
+            </div>    
+          </div>
+          <!-- End: Video Widget-->
+        </div>
+
+        <!-- left side video 2 of 2 -->
+        <div class="col-sm-6">
+          <!-- Begin: Video Widget-->  
+          <div class="widget-video">
+            <div class="video-slides">
+              <div class="slide slide-front">
+                <div class="image">
+                  <img src="images/brand-pages/conex/video-placeholder-2.jpg" alt="Specialist O-ring">
+                </div>
+                <div class="slide-content">
+                  <button type="button" class="play" data-video="_0TBVfMLoc8">
+                    <i class="rbp-icon"></i>
+                    <!--<span class="label"></span>  -->
+                  </button>  
+                </div>
+              </div>
+              <div class="slide slide-video">
+                <div class="video-wrapper">
+                  <div class="video">
+                    <figure class="player"></figure>
+                  </div>
+                </div>
+                <button type="button" class="close">
+                  <i class="rbp-close-icon"></i>               
+                </button>  
+              </div>
+            </div>    
+          </div>
+          <!-- End: Video Widget-->
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- DOWNLOADS SECTION -->  
+  <div class="container-fluid downloads">
+    <div class="row">
+      <div class="col-xs-12">
         <div class="widget-html-image">
-          <figure class="image">
-            <!--<a href="#">-->
-            <img src="images/brand-pages/conex/b-press-xl.jpg" alt="B Press XL 65-100mm"/>
-            <figcaption>B Press XL 65-100mm</figcaption>
-            <!--</a>-->
-          </figure>
+          <div class="rte">
+            <h3><!--<a href="#">-->DOWNLOAD<!--</a>--></h3>
+          </div>
         </div>
         <!-- End: HTML Widget-->
       </div>
     </div>
   </div>
 
-  <div class="container-fluid downloads">
+ <div class="container-fluid downloads">
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-xs-12 ">
         <!-- Begin: HTML Widget-->  
-        <div class="widget-big-button videos">
-          <div class="rte">
-            <a href="#"><span class="icon"><i class="rbp-icon"></i></span>
-            <span class="text">Videos</span></a>
-          </div>            
+        <div class="widget-html-image">
+          <figure class="image">
+            <a href="#"><!-- Link Optional-->
+              <img src="images/brand-pages/conex/download-thumb1.jpg" height="240px" alt="download">
+            </a>
+          </figure>
         </div>
-        <!-- End: HTML Widget-->
-      </div>
-      <div class="col-sm-4">
-        <!-- Begin: HTML Widget-->  
-        <div class="widget-big-button download">
-          <div class="rte">
-            <a href="#"><span class="icon"><i class="rbp-icon"></i></span><span class="text">Download Brochure</span></a>
-          </div>            
+
+        <div class="widget-html-image">
+          <figure class="image">
+            <a href="#"><!-- Link Optional-->
+              <img src="images/brand-pages/conex/download-thumb2.jpg" height="240px" alt="download">
+            </a>
+          </figure>
         </div>
-        <!-- End: HTML Widget-->
-      </div>
-      <div class="col-sm-4">
-        <!-- Begin: HTML Widget-->  
-        <div class="widget-big-button website">
-          <div class="rte">
-            <a href="#"><span class="icon"><i class="rbp-icon"></i></span><span class="text">Visit Website</span></a>
-          </div>            
+
+          <figure class="image">
+            <a href="#"><!-- Link Optional-->
+              <img src="images/brand-pages/conex/download-thumb3.jpg" height="240px" alt="download">
+            </a>
+          </figure>
         </div>
-        <!-- End: HTML Widget-->
       </div>
+        <!-- End: HTML Widget-->
     </div>
   </div>
+</div>
 </div>

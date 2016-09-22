7fca9df6f497186405196ffcc9bef108f9d70d67
Diego Pelaez <diego@trout.com.au>
2016-09-22 14:59:07 +1000

    Add live font and remove testing font.

--- bathroom-happiness/templates/includes/reece-head.php
+++ bathroom-happiness/templates/includes/reece-head.php
@@ -6,6 +6,7 @@
     <!--[if lt IE 9]>
         <script src="js/vendor/html5shiv/dist/html5shiv.js"></script>
     <![endif]-->
+    <link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKit.css" />
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/reece-global.css" />
     <script src='js/vendor/jquery.min.js' type='text/javascript'></script>
@@ -16,17 +17,5 @@
     <script src='js/vendor/jquery.validate.min.js' type='text/javascript'></script>
     <?php if(isset($extra_javascript)){ echo $extra_javascript;}?>
     <script src='js/vendor/placeholders.min.js' type='text/javascript'></script>
-    <script src='js/src/main.js' type='text/javascript'></script>
-    <script type="text/javascript">
-    (function() {
-        var path = '//easy.myfonts.net/v2/js?sid=289419(font-family=Handelson+Three)&key=EtuFBAx1Ax',
-            protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
-            trial = document.createElement('script');
-        trial.type = 'text/javascript';
-        trial.async = true;
-        trial.src = protocol + path;
-        var head = document.getElementsByTagName("head")[0];
-        head.appendChild(trial);
-    })();
-    </script>
+    <script src='js/src/main.js' type='text/javascript'></script>    
   </head>
